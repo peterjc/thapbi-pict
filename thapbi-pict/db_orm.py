aa49249cdbf7ba4de02e4f9419f1816897fb4bc8
peterjc <p.j.a.cock@googlemail.com>
2018-11-06 11:11:14 +0000

    Clarify species recording after discussion with LP

--- thapbi-pict/db_orm.py
+++ thapbi-pict/db_orm.py
@@ -5,7 +5,7 @@ database schema and the code to import/export the data as
 Python objects.
 """
 
-from sqlalchemy import Column, Integer, String, Date, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -36,10 +36,22 @@ class SequenceSource(Base):
     its1_md5 = Column(String(32), ForeignKey("its1_sequence.md5"))
     sequence = Column(String(1000))  # Full sequence, can be longer than ITS1
 
-    taxid = Column(Integer)  # NCBI taxid
-    # species = Column(String(100))  # Assume will have an NCBI taxid
+    # Whatever was recorded in the original data source
+    original_taxid = Column(Integer)  # NCBI taxid
+    original_genus = Column(String(100))
+    original_species = Column(String(100))
 
-    date_added = Column(Date)  # have data_modified too?
+    # Initially the same values as above, but expect to reclassify some entries
+    current_taxid = Column(Integer)  # NCBI taxid
+    current_genus = Column(String(100))
+    current_species = Column(String(100))
+
+    # TODO - Supplement this with an NCBI taxonomy table?
+    # i.e. looking up species name and genus via the taxid
+    # For now, storing genus etc locally will help with simple filtering
+
+    date_added = Column(DateTime)
+    date_modified = Column(DateTime)
 
     # Implement as an enum?
     # Unknown, Public DB, Direct Sequencing, Genome Sequence, Metabarcoding
