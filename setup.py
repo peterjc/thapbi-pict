215b2fe5129ce1acbe00327c13b88fac388c2145
peterjc <p.j.a.cock@googlemail.com>
2018-11-07 11:09:02 +0000

    Minimal setup.py file

---
+++ setup.py
@@ -0,0 +1,87 @@
+"""setuptools based setup script for THAPBI PICT.
+
+This uses setuptools which is now the standard python mechanism for installing
+packages (and is used internally by the tool pip). If you have downloaded and
+uncompressed the THAPBI PICT source code, or fetched it from git, for the
+simplest installation try one of these commands::
+
+    python3 setup.py install
+
+Or::
+
+    pip3 install .
+
+In future we intend to release this software on PyPI to allow you to install
+it with pip without having to download it first::
+
+    pip3 install thapbi_pict
+
+"""
+from __future__ import print_function
+
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    sys.exit("We need the Python library setuptools to be installed. "
+             "Try runnning: python -m ensurepip")
+
+
+# Make sure we have the right Python version.
+if sys.version_info[:2] < (3, 5):
+    sys.exit("THAPBI PICT requires Python 3.5 or later. "
+             "Python %d.%d detected.\n" % sys.version_info[:2])
+
+# We define the version number in thapbi_pict/__init__.py
+# Here we can't use "import thapbi_pict" then "thapbi_pict.__version__"
+# as that would tell us the version already installed (if any).
+__version__ = "Undefined"
+for line in open('thapbi_pict/__init__.py'):
+    if (line.startswith('__version__')):
+        exec(line.strip())
+
+# Load our markdown file README.md as the long description.
+# Must tell PyPI this is not using reStructuredText
+#
+# Without declaring an encoding, any problematic character in the file, it may
+# fail on Python 3 depending on the user's locale. By explicitly checking it
+# it is ASCII (could use latin1 or UTF8 if needed later), if any invalid
+# character does appear in our README, this will fail for everyone.
+with open("README.md", "rb") as handle:
+    readme_md = handle.read().decode("ascii")
+
+setup(name='thapbi_pict',
+      version=__version__,
+      author='Peter Cock',
+      author_email='peter.cock@hutton.ac.uk',
+      url='https://github.com/peterjc/thapbi-pict',  # For now at least
+      download_url='https://github.com/peterjc/thapbi-pict',
+      description='THAPBI Phytophthora ITS1 Classifier Tool (PICT).',
+      long_description=readme_md,
+      long_description_content_type='text/markdown',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: Science/Research',
+          'License :: Freely Distributable',
+          'License :: OSI Approved :: MIT License',
+          'Natural Language :: English',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: POSIX :: Linux',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Topic :: Scientific/Engineering',
+          'Topic :: Scientific/Engineering :: Bio-Informatics',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
+      packages=[
+          'thapbi_pict',
+      ],
+      install_requires=[
+          'biopython',
+          'sqlalchemy',
+      ],
+      )
