214b4959dc8e0115eb31b8fed74ca536879b6c67
peterjc <p.j.a.cock@googlemail.com>
2018-11-07 09:59:56 +0000

    Define SQLalchemy session

--- thapbi_pict/db_orm.py
+++ thapbi_pict/db_orm.py
@@ -8,10 +8,12 @@ Python objects.
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 
 engine = create_engine('sqlite:///:memory:', echo=True)
 Base = declarative_base()
+Session = sessionmaker(bind=engine)
 
 
 class ITS1(Base):
