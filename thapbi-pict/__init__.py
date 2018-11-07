96eac9d177ee57ece71cec2fe9ad0feff760fc03
peterjc <p.j.a.cock@googlemail.com>
2018-11-07 10:47:07 +0000

    Add description to the top level Python pacakge file

--- thapbi-pict/__init__.py
+++ thapbi-pict/__init__.py
@@ -1,3 +1,16 @@
-"""Python code for THAPBI PICT."""
+"""THAPBI *Phytophthora* ITS1 Classifier Tool (PICT).
+
+*Phytophthora* (from Greek meaning plant-destroyer) species are economically
+important plant pathogens, important in both agriculture and forestry. ITS1 is
+short for Internal Transcribed Spacer one, which is a region of eukaryotes
+genomes between the 18S and 5.8S rRNA genes. This is commonly used for
+molecular barcoding, where sequencing this short region can identify species.
+
+This Python package provides ITS1-based diagnostic/profiling tools for the
+Tree Health and Plant Biosecurity Initiative (THAPBI) Phyto-Threats project,
+funded by the UK's [BBSRC](https://www.bbsrc.ac.uk).
+
+You would typically use THAPBI PICT via the command line tools it defines.
+"""
 
 __version__ = "0.0.0"
