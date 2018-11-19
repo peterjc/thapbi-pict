04129677bac4c30cdd813d830e8d2c26a4557dcb
peterjc <p.j.a.cock@googlemail.com>
2018-11-19 16:09:12 +0000

    Start of test case for ncbi-import

---
+++ tests/test_ncbi-import.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+IFS=$'\n\t'
+set -eux
+
+# Note not using "set -o pipefile" as want to use that
+# with grep to check error messages
+
+export TMP=${TMP:-/tmp}
+
+echo "Checking ncbi-import"
+thapbi_pict ncbi-import 2>&1 | grep "the following arguments are required"
+
+if [ ! -f $TMP/ncbi_sample.fasta ]; then esearch -db nucleotide -query "its1 AND Phytophthora[Organism] AND 150:800[Sequence Length]" | efetch -format fasta > $TMP/ncbi_sample.fasta; fi
+
+grep -c "^>" $TMP/ncbi_sample.fasta
+
+rm -rf $TMP/ncbi_sample.sqlite
+thapbi_pict ncbi-import -d $TMP/ncbi_sample.sqlite $TMP/ncbi_sample.fasta
+
+if [ `sqlite3 $TMP/ncbi_sample.sqlite "SELECT COUNT(id) FROM data_source;"` == "1" ]; then true; else echo "Wrong data_source count"; false; fi
+
+thapbi_pict dump 2>&1 | grep "the following arguments are required"
+thapbi_pict dump -d $TMP/ncbi_sample.sqlite -o /dev/null
+
+echo "$0 passed"
