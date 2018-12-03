fd5bdb0fe54c71991f45d5757b7f3d108faf893e
peterjc <p.j.a.cock@googlemail.com>
2018-12-03 12:11:15 +0000

    Test thapbi_pict load-tax

--- tests/test_load-tax.sh
+++ tests/test_load-tax.sh
@@ -13,3 +13,4 @@ thapbi_pict load-tax 2>&1 | grep "the following arguments are required"
 if [ ! -f "new_taxdump_2018-12-01.zip" ]; then curl -L -O "ftp://ftp.ncbi.nih.gov/pub/taxonomy/taxdump_archive/new_taxdump_2018-12-01.zip"; fi
 if [ ! -d "new_taxdump_2018-12-01" ]; then unzip new_taxdump_2018-12-01.zip -d new_taxdump_2018-12-01; fi
 
+thapbi_pict load-tax -d "sqlite:///:memory:" -t new_taxdump_2018-12-01 -v
