9910e627c8d02e9e7c480f1de4f0fbdfe4a79327
peterjc <p.j.a.cock@googlemail.com>
2018-12-05 15:58:01 +0000

    Must download taxdump before this test

--- tests/test_load-tax.sh
+++ tests/test_load-tax.sh
@@ -10,9 +10,6 @@ export TMP=${TMP:-/tmp}
 echo "Checking load-tax"
 thapbi_pict load-tax 2>&1 | grep "the following arguments are required"
 
-# Check this error condition
-thapbi_pict load-tax -d sqlite:///:memory: -t taxdmp_2014-08-01 -a 12908 2>&1 | grep "Could not identify any genus names"
-
 if [ ! -f "new_taxdump_2018-12-01.zip" ]; then curl -L -O "https://ftp.ncbi.nih.gov/pub/taxonomy/taxdump_archive/new_taxdump_2018-12-01.zip"; fi
 if [ ! -d "new_taxdump_2018-12-01" ]; then unzip new_taxdump_2018-12-01.zip -d new_taxdump_2018-12-01; fi
 
@@ -29,3 +26,6 @@ thapbi_pict load-tax -d sqlite:///:memory: -t taxdmp_2014-08-01 -a 4783,70742
 
 # Request all Peronosporales
 thapbi_pict load-tax -d sqlite:///:memory: -t taxdmp_2014-08-01 -a 4776
+
+# Check this error condition
+thapbi_pict load-tax -d sqlite:///:memory: -t taxdmp_2014-08-01 -a 12908 2>&1 | grep "Could not identify any genus names"
