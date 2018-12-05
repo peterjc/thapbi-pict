11d32918f75742ba54ef0d14a0c1babebf0f666a
peterjc <p.j.a.cock@googlemail.com>
2018-12-05 12:45:26 +0000

    Use HTTPS not FTP for taxonomy test - more reliable
    This should solve the recent timeouts on TravisCI,
    having seen this problem before with other projects.

--- tests/test_load-tax.sh
+++ tests/test_load-tax.sh
@@ -10,12 +10,12 @@ export TMP=${TMP:-/tmp}
 echo "Checking load-tax"
 thapbi_pict load-tax 2>&1 | grep "the following arguments are required"
 
-if [ ! -f "new_taxdump_2018-12-01.zip" ]; then curl -L -O "ftp://ftp.ncbi.nih.gov/pub/taxonomy/taxdump_archive/new_taxdump_2018-12-01.zip"; fi
+if [ ! -f "new_taxdump_2018-12-01.zip" ]; then curl -L -O "https://ftp.ncbi.nih.gov/pub/taxonomy/taxdump_archive/new_taxdump_2018-12-01.zip"; fi
 if [ ! -d "new_taxdump_2018-12-01" ]; then unzip new_taxdump_2018-12-01.zip -d new_taxdump_2018-12-01; fi
 
 thapbi_pict load-tax -d "sqlite:///:memory:" -t new_taxdump_2018-12-01
 
-if [ ! -f "taxdmp_2014-08-01.zip" ]; then curl -L -O "ftp://ftp.ncbi.nih.gov/pub/taxonomy/taxdump_archive/taxdmp_2014-08-01.zip"; fi
+if [ ! -f "taxdmp_2014-08-01.zip" ]; then curl -L -O "https://ftp.ncbi.nih.gov/pub/taxonomy/taxdump_archive/taxdmp_2014-08-01.zip"; fi
 if [ ! -d "taxdmp_2014-08-01" ]; then unzip taxdmp_2014-08-01.zip -d taxdmp_2014-08-01; fi
 
 # Defaults to all Phytophthora
