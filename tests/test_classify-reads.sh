8dc2370ae9992de74f0eaa92a033370650887565
peterjc <p.j.a.cock@googlemail.com>
2018-12-10 11:35:28 +0000

    Use test DB variable name; check it exists

--- tests/test_classify-reads.sh
+++ tests/test_classify-reads.sh
@@ -11,10 +11,13 @@ echo "Checking classify-reads"
 thapbi_pict classify-reads 2>&1 | grep "the following arguments are required"
 thapbi_pict classify-reads -d "sqlite:///:memory:" hypothetical_example.fasta 2>&1 | grep "cannot classify anything"
 
+export DB=$TMP/legacy_004_and_005_validated.sqlite
+if [ ! -f $DB ]; then echo "Run test_legacy-import.sh to setup test DB"; false; fi
+
 # Passing one filename:
-thapbi_pict classify-reads -m identity -d $TMP/legacy_004_and_005_validated.sqlite database/legacy/database.fasta
+thapbi_pict classify-reads -m identity -d $DB database/legacy/database.fasta
 
 # Passing one directory name (should get all three FASTA files):
-thapbi_pict classify-reads -m identity -d $TMP/legacy_004_and_005_validated.sqlite database/legacy/
+thapbi_pict classify-reads -m identity -d $DB database/legacy/
 
 echo "$0 passed"
