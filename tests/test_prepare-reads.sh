8361b3745a2672f2f78178b980ea4973a76a344c
peterjc <p.j.a.cock@googlemail.com>
2018-12-13 09:58:14 +0000

    Use DNAMIX_S95_L001_*.fastq.gz to test prepare-reads

--- tests/test_prepare-reads.sh
+++ tests/test_prepare-reads.sh
@@ -10,5 +10,9 @@ export TMP=${TMP:-/tmp}
 echo "Checking prepare-reads"
 thapbi_pict prepare-reads 2>&1 | grep "the following arguments are required"
 
+# Try a real example
+rm -rf $TMP/DNAMIX_S95_L001.prepared.fasta
+thapbi_pict prepare-reads -o $TMP tests/reads/DNAMIX_S95_L001_*.fastq.gz
+if [ `grep -c "^>" $TMP/DNAMIX_S95_L001.prepared.fasta` -ne "4710" ]; then echo "Wrong FASTA output count"; false; fi
 
 echo "$0 passed"
