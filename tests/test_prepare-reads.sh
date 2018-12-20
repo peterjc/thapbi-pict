4ea5fcafc5d4976ae27322ec0aa0fba3e4859432
peterjc <p.j.a.cock@googlemail.com>
2018-12-20 14:16:02 +0000

    Update test_prepare-reads.sh for min-abundance

--- tests/test_prepare-reads.sh
+++ tests/test_prepare-reads.sh
@@ -12,7 +12,11 @@ thapbi_pict prepare-reads 2>&1 | grep "the following arguments are required"
 
 # Try a real example
 rm -rf $TMP/DNAMIX_S95_L001.prepared.fasta
-thapbi_pict prepare-reads -o $TMP tests/reads/DNAMIX_S95_L001_*.fastq.gz
+thapbi_pict prepare-reads -o $TMP tests/reads/DNAMIX_S95_L001_*.fastq.gz -a 0
 if [ `grep -c "^>" $TMP/DNAMIX_S95_L001.prepared.fasta` -ne "892" ]; then echo "Wrong FASTA output count"; false; fi
+
+rm -rf $TMP/DNAMIX_S95_L001.prepared.fasta
+thapbi_pict prepare-reads -o $TMP tests/reads/DNAMIX_S95_L001_*.fastq.gz -a 100
+if [ `grep -c "^>" $TMP/DNAMIX_S95_L001.prepared.fasta` -ne "7" ]; then echo "Wrong FASTA output count"; false; fi
 
 echo "$0 passed"
