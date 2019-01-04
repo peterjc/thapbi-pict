64b85ba2aef3f2c50e9dd2f1e0703ed3042861f4
peterjc <p.j.a.cock@googlemail.com>
2019-01-04 13:55:34 +0000

    Refactor filter_for_ITS1, returns single match
    Can still return None, but will not use a list anymore
    as now return a single ITS1 sequence using the span of
    the HMM hits.

--- thapbi_pict/import_fasta.py
+++ thapbi_pict/import_fasta.py
@@ -176,23 +176,19 @@ def import_fasta_file(fasta_file, db_url, name=None, debug=True,
     good_entries = 0
     idn_set = set()
 
-    for title, seq, its1_seqs in filter_for_ITS1(fasta_file):
+    for title, seq, its1_seq in filter_for_ITS1(fasta_file):
         seq_count += 1
         if title.startswith("Control_"):
             if debug:
                 sys.stderr.write("DEBUG: Ignoring control entry: %s\n"
                                  % title)
             continue
-        if not its1_seqs:
+        if not its1_seq:
             if debug:
                 sys.stderr.write("DEBUG: Ignoring non-ITS entry: %s\n"
                                  % title)
             continue
-        if len(its1_seqs) > 1:
-            sys.stderr.write("WARNING: Ignoring %s as has multiple HSPs\n"
-                             % title)
-            continue
-        its1_seq = its1_seqs[0]
+
         its1_seq_count += 1
 
         its1_md5 = hashlib.md5(its1_seq.upper().encode("ascii")).hexdigest()
