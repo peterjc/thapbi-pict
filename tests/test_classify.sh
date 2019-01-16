a11dfa946747be330c4bc7bfaa7767e2c9c4549d
peterjc <p.j.a.cock@googlemail.com>
2019-01-16 18:01:14 +0000

    More cleanup from interupted commit

--- tests/test_classify.sh
+++ tests/test_classify.sh
@@ -7,9 +7,9 @@ set -eux
 
 export TMP=${TMP:-/tmp}
 
-echo "Checking classify-reads"
-thapbi_pict classify-reads 2>&1 | grep "the following arguments are required"
-thapbi_pict classify-reads -d "sqlite:///:memory:" hypothetical_example.fasta 2>&1 | grep "cannot classify anything"
+echo "Checking classify"
+thapbi_pict classify 2>&1 | grep "the following arguments are required"
+thapbi_pict classify -d "sqlite:///:memory:" hypothetical_example.fasta 2>&1 | grep "cannot classify anything"
 
 export DB=$TMP/legacy_004_and_005_validated.sqlite
 if [ ! -f $DB ]; then echo "Run test_legacy-import.sh to setup test DB"; false; fi
@@ -18,7 +18,7 @@ rm -rf database/legacy/*.identity-reads.tsv
 rm -rf database/legacy/*.identity-tax.tsv
 
 # Passing one filename; default output dir:
-thapbi_pict classify-reads -m identity -d $DB database/legacy/database.fasta
+thapbi_pict classify -m identity -d $DB database/legacy/database.fasta
 # Now fails as we expect reads to have been prepared and trimmed with HMM
 if [ `wc -l database/legacy/database.identity-reads.tsv` -ne "0" ]; then echo "Expected no matches"; false; fi
 
@@ -26,14 +26,14 @@ if [ ! -f $TMP/DNAMIX_S95_L001.prepared.fasta ]; then echo "Run test_prepare-rea
 rm -rf $TMP/DNAMIX_S95_L001.prepared.swarm-reads.tsv
 rm -rf $TMP/DNAMIX_S95_L001.prepared.swarm-tax.tsv
 
-thapbi_pict classify-reads -m swarm -d $DB $TMP/DNAMIX_S95_L001.prepared.fasta
+thapbi_pict classify -m swarm -d $DB $TMP/DNAMIX_S95_L001.prepared.fasta
 cut -f 5 $TMP/DNAMIX_S95_L001.prepared.swarm-reads.tsv | sort | uniq -c
 # grep -c Phytophthora $TMP/DNAMIX_S95_L001.prepared.swarm-tax.tsv
 
 # Passing one directory name (should get all three FASTA files):
 rm -rf $TMP/legacy/*.identity-*.tsv
 mkdir -p $TMP/legacy
-thapbi_pict classify-reads -m identity -d $DB database/legacy/ -o $TMP/legacy
+thapbi_pict classify -m identity -d $DB database/legacy/ -o $TMP/legacy
 ls -1 $TMP/legacy/*.identity-*.tsv  # Should be 6 pairs
 
 echo "$0 passed"
