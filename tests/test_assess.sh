c8098e4f1c39af20b658c89fc9800dfe813f8396
peterjc <p.j.a.cock@googlemail.com>
2019-01-22 17:23:06 +0000

    Tweak test

--- tests/test_assess.sh
+++ tests/test_assess.sh
@@ -9,24 +9,21 @@ export TMP=${TMP:-/tmp}
 
 echo "Checking assess"
 
-if [ ! -f $TMP/DNAMIX_S95_L001.swarm.tsv ]; then echo "Run test_classify.sh to setup test input"; false; fi
+if [ ! -f $TMP/thapbi_swarm/DNAMIX_S95_L001.swarm.tsv ]; then echo "Run test_classify.sh to setup test input"; false; fi
 if [ ! -f $TMP/DNAMIX_S95_L001.identity.tsv ]; then echo "Run test_classify.sh to setup test input"; false; fi
 
 rm -rf $TMP/assess_swarm_vs_identity.tsv
 rm -rf $TMP/confusion_swarm_vs_identity.tsv
 
 # Don't have a gold standard known truth to test this against, so test swarm vs identity
-thapbi_pict assess $TMP/DNAMIX_S95_L001.swarm.tsv $TMP/DNAMIX_S95_L001.identity.tsv -m swarm -k identity -o $TMP/assess_swarm_vs_identity.tsv -c $TMP/confusion_swarm_vs_identity.tsv
+thapbi_pict assess $TMP/thapbi_swarm/DNAMIX_S95_L001.swarm.tsv $TMP/DNAMIX_S95_L001.identity.tsv -m swarm -k identity -o $TMP/assess_swarm_vs_identity.tsv -c $TMP/confusion_swarm_vs_identity.tsv
 
 # Check assessment output to stdout works (default):
-thapbi_pict assess $TMP/DNAMIX_S95_L001.swarm.tsv $TMP/DNAMIX_S95_L001.identity.tsv -m swarm -k identity > $TMP/stdout.txt
+thapbi_pict assess $TMP/thapbi_swarm/DNAMIX_S95_L001.swarm.tsv $TMP/DNAMIX_S95_L001.identity.tsv -m swarm -k identity > $TMP/stdout.txt
 diff $TMP/stdout.txt $TMP/assess_swarm_vs_identity.tsv
 
-# Check confusion matrix output to stdout works:
-thapbi_pict assess $TMP/DNAMIX_S95_L001.swarm.tsv $TMP/DNAMIX_S95_L001.identity.tsv -m swarm -k identity -o $TMP/assess_swarm_vs_identity.tsv -c - > $TMP/stdout.txt
+# Check confusion matrix output to stdout works, also give one input as a directory name
+thapbi_pict assess $TMP/thapbi_swarm $TMP/DNAMIX_S95_L001.identity.tsv -m swarm -k identity -o $TMP/assess_swarm_vs_identity.tsv -c - > $TMP/stdout.txt
 diff $TMP/stdout.txt $TMP/confusion_swarm_vs_identity.tsv
-
-# Test with two files in different input folders
-thapbi_pict assess $TMP/DNAMIX_S95_L001.identity.tsv $TMP/thapbi_swarm/ -m swarm -k identity -o $TMP/assess_swarm_vs_identity.tsv -c $TMP/confusion_swarm_vs_identity.tsv
 
 echo "$0 passed"
