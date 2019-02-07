352c6433e808651a947e46806d6834d17f0b9bd6
peterjc <p.j.a.cock@googlemail.com>
2019-02-07 15:14:45 +0000

    Clarify thapbi_pict assess command line help

--- thapbi_pict/__main__.py
+++ thapbi_pict/__main__.py
@@ -524,11 +524,11 @@ comma.
     parser_assess = subparsers.add_parser(
         "assess",
         description="Assess accuracy of ITS1 read classification.",
-        epilog="Takes as input prediction in XXX.method.tsv "
+        epilog="Takes as input predictions named XXX.method.tsv "
         "and matching expected classifications in XXX.known.tsv "
         "(which can be in different directories) to produce a "
-        "multi-species confusion matrix named XXX.method-vs-known.tsv, "
-        "and a summary to stdout. "
+        "multi-species confusion matrix (output on request) and "
+        "classifier performance metrics (to stdout by default)."
         "You can deliberately compare two prediction methods to "
         "each other using this, but a known set of positive controls "
         "is the expected benchmark.",
