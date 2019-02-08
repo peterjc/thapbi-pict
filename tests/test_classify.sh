859c072f0af433abb77f44fe46688b92bb202f4b
peterjc <p.j.a.cock@googlemail.com>
2019-02-08 15:38:50 +0000

    Test thapbi_pict classify -m onebp

--- tests/test_classify.sh
+++ tests/test_classify.sh
@@ -21,13 +21,16 @@ if [ "`grep -c -v '^#' database/legacy/database.identity.tsv`" -ne "`grep -c '^>
 
 if [ ! -f $TMP/DNAMIX_S95_L001.fasta ]; then echo "Run test_prepare-reads.sh to setup test input"; false; fi
 rm -rf $TMP/DNAMIX_S95_L001.identity.tsv
+rm -rf $TMP/thapbi_onebp
 rm -rf $TMP/thapbi_swarm
 rm -rf $TMP/thapbi_blast
+mkdir -p $TMP/thapbi_onebp
 mkdir -p $TMP/thapbi_swarm
 mkdir -p $TMP/thapbi_blast
 
 # Explicitly setting output directory, would be here anyway:
 thapbi_pict classify -m identity -d $DB $TMP/DNAMIX_S95_L001.fasta -o $TMP/
+thapbi_pict classify -m onebp -d $DB $TMP/DNAMIX_S95_L001.fasta -o $TMP/thapbi_onebp
 thapbi_pict classify -m blast -d $DB $TMP/DNAMIX_S95_L001.fasta -o $TMP/thapbi_blast
 thapbi_pict classify -m swarm -d $DB $TMP/DNAMIX_S95_L001.fasta -o $TMP/thapbi_swarm
 cut -f 5 $TMP/thapbi_swarm/DNAMIX_S95_L001.swarm.tsv | sort | uniq -c
