65c26517853b92b56aeb74b58f1497ee92932575
peterjc <p.j.a.cock@googlemail.com>
2019-02-11 13:53:22 +0000

    Per-sequence reporting at plate level

--- thapbi_pict/summary.py
+++ thapbi_pict/summary.py
@@ -6,8 +6,14 @@ This implements the ``thapbi_pict plate-summary ...`` command.
 import sys
 import tempfile
 
+from collections import Counter
+
+from Bio.SeqIO.FastaIO import SimpleFastaParser
 
 from .utils import find_paired_files
+from .utils import parse_species_tsv
+from .utils import split_read_name_abundance
+from .utils import untangle_species
 
 
 def main(inputs, output, method, min_abundance=1, debug=False):
@@ -29,8 +35,63 @@ def main(inputs, output, method, min_abundance=1, debug=False):
     with tempfile.TemporaryDirectory() as shared_tmp:
         if debug:
             sys.stderr.write("DEBUG: Shared temp folder %s\n" % shared_tmp)
+
+        md5_abundance = Counter()
+        abundance_by_samples = dict()
+        md5_species = dict()
+        md5_to_seq = dict()
+
         for fasta_file, predicted_file in input_list:
             if debug:
                 sys.stderr.write(
                     "DEBUG: Reading %s %s\n" % (fasta_file, predicted_file)
                 )
+            sample = fasta_file.rsplit(".", 1)[0]
+            # Load the TSV
+            for name, taxid, genus, species in parse_species_tsv(
+                predicted_file, min_abundance
+            ):
+                md5, abundance = split_read_name_abundance(name)
+                sp_list = untangle_species(taxid, genus, species).split(";")
+                try:
+                    md5_species[md5].union(sp_list)
+                except KeyError:
+                    md5_species[md5] = set(sp_list)
+                abundance_by_samples[md5, sample] = abundance
+                md5_abundance[md5] += abundance
+            # Load the FASTA
+            with open(fasta_file) as handle:
+                for title, seq in SimpleFastaParser(handle):
+                    md5, abundance = split_read_name_abundance(title.split(None, 1)[0])
+                    assert abundance_by_samples[md5, sample] == abundance
+                    md5_to_seq[md5] = seq
+
+        if output == "-":
+            if debug:
+                sys.stderr.write("DEBUG: Output to stdout...\n")
+            handle = sys.stdout
+        else:
+            handle = open(output, "w")
+
+        handle.write("#ITS1-MD5\tTotal-abundance\t%s-predictions\n" % method)
+        for total_abundance, md5 in reversed(
+            sorted((v, k) for (k, v) in md5_abundance.items())
+        ):
+            handle.write(
+                "%s\t%i\t%s\n"
+                % (md5, total_abundance, ";".join(sorted(md5_species[md5])))
+            )
+
+        if output != "-":
+            handle.close()
+
+        try:
+            sys.stdout.flush()
+        except BrokenPipeError:
+            pass
+        try:
+            sys.stderr.flush()
+        except BrokenPipeError:
+            pass
+
+        return 0
