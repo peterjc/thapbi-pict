28195974cb83656c97cb300b13f2f7ff131de7a2
peterjc <p.j.a.cock@googlemail.com>
2019-03-18 10:40:11 +0000

    Fix missing return information in docstring
    [ci skip]

--- thapbi_pict/prepare.py
+++ thapbi_pict/prepare.py
@@ -230,6 +230,7 @@ def filter_fasta_for_its1(input_fasta, output_fasta, stem, shared_tmp_dir, debug
     Assumes the SWARM naming convention.
 
     Returns the number of unique ITS1 sequences (integer),
+    maximum abundance, and number of cropping warnings.
     """
     exp_left = 32
     exp_right = 0
