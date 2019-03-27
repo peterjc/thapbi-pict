7af53e41b569fa167120a652945c3b226357d773
peterjc <p.j.a.cock@googlemail.com>
2019-03-27 09:58:28 +0000

    Minimal tests for sample-summary

---
+++ tests/test_sample-summary.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+IFS=$'\n\t'
+set -eux
+# Note not using "set -o pipefile" until after check error message with grep
+
+export TMP=${TMP:-/tmp}
+
+echo "Checking sample-summary"
+thapbi_pict sample-summary 2>&1 | grep "the following arguments are required"
+set -o pipefail
+
+# Passing filename, default method, explicit min abundance
+rm -rf $TMP/human.txt $TMP/computer.tsv
+thapbi_pict sample-summary -a 1 -r $TMP/human.txt -o $TMP/computer.tsv tests/classify/*.identity.tsv
+diff $TMP/human.txt tests/sample-summary/classify.identity.txt
+diff $TMP/computer.tsv tests/sample-summary/classify.identity.tsv
+
+# Passing a folder, trying different methods
+for M in identity onebp blast swarmid swarm; do
+    rm -rf $TMP/human.txt $TMP/computer.tsv
+    thapbi_pict sample-summary -m $M -r $TMP/human.txt -o $TMP/computer.tsv tests/classify/
+    diff $TMP/human.txt tests/sample-summary/classify.$M.txt
+    diff $TMP/computer.tsv tests/sample-summary/classify.$M.tsv
+done
+
+echo "$0 - test_sample-summary.sh passed"
