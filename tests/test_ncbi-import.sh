3af902aa9793c04158383cb213f044a59037b8ef
peterjc <p.j.a.cock@googlemail.com>
2019-04-03 09:27:49 +0100

    Update test, load-tax no longer defaults to just Phytophthora

--- tests/test_ncbi-import.sh
+++ tests/test_ncbi-import.sh
@@ -51,12 +51,12 @@ thapbi_pict dump -d $DB -o /dev/null -g Phytophthora -s "ilicis, sp. aff. meadii
 export DB=$TMP/20th_Century_ITS1_genus_only.sqlite
 rm -rf $DB
 thapbi_pict load-tax -d $DB -t new_taxdump_2018-12-01
-if [ `sqlite3 $DB "SELECT COUNT(id) FROM taxonomy;"` -ne "252" ]; then echo "Wrong taxonomy count"; false; fi
+if [ `sqlite3 $DB "SELECT COUNT(id) FROM taxonomy;"` -ne "595" ]; then echo "Wrong taxonomy count"; false; fi
 thapbi_pict ncbi-import -d $DB tests/ncbi-import/20th_Century_ITS1.fasta -g
 if [ `sqlite3 $DB "SELECT COUNT(id) FROM data_source;"` -ne "1" ]; then echo "Wrong data_source count"; false; fi
 if [ `sqlite3 $DB "SELECT COUNT(id) FROM its1_source;"` -ne "106" ]; then echo "Wrong its1_source count"; false; fi
 if [ `sqlite3 $DB "SELECT COUNT(id) FROM its1_sequence;"` -ne "89" ]; then echo "Wrong its1_sequence count"; false; fi
-if [ `sqlite3 $DB "SELECT COUNT(id) FROM taxonomy;"` -ne "252" ]; then echo "Wrong taxonomy count"; false; fi
+if [ `sqlite3 $DB "SELECT COUNT(id) FROM taxonomy;"` -ne "595" ]; then echo "Wrong taxonomy count"; false; fi
 if [ `thapbi_pict dump -d $DB | grep -c 4783` -ne "106" ]; then echo "Should all be just genus"; false; fi
 
 echo "$0 - test_ncbi-import.sh passed"
