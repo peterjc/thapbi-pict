a06ffa2c2c8a360ced77d229e92696f6252f762c
peterjc <p.j.a.cock@googlemail.com>
2019-04-03 16:07:23 +0100

    Add a proper test for importing sequences at genus level only

--- tests/test_ncbi-import.sh
+++ tests/test_ncbi-import.sh
@@ -18,7 +18,6 @@ set -o pipefail
 export DB=$TMP/20th_Century_ITS1.sqlite
 rm -rf $DB
 thapbi_pict ncbi-import -x -d $DB tests/ncbi-import/20th_Century_ITS1.fasta
-
 if [ `sqlite3 $DB "SELECT COUNT(id) FROM data_source;"` -ne "1" ]; then echo "Wrong data_source count"; false; fi
 if [ `sqlite3 $DB "SELECT COUNT(id) FROM its1_source;"` -ne "107" ]; then echo "Wrong its1_source count"; false; fi
 if [ `sqlite3 $DB "SELECT COUNT(id) FROM its1_sequence;"` -ne "90" ]; then echo "Wrong its1_sequence count"; false; fi
@@ -58,5 +57,26 @@ if [ `sqlite3 $DB "SELECT COUNT(id) FROM its1_source;"` -ne "106" ]; then echo "
 if [ `sqlite3 $DB "SELECT COUNT(id) FROM its1_sequence;"` -ne "89" ]; then echo "Wrong its1_sequence count"; false; fi
 if [ `sqlite3 $DB "SELECT COUNT(id) FROM taxonomy;"` -ne "595" ]; then echo "Wrong taxonomy count"; false; fi
 if [ `thapbi_pict dump -d $DB | grep -c 4783` -ne "106" ]; then echo "Should all be just genus"; false; fi
+
+# Now using the Phytophthora at species level, Peronosporaceae at genus level:
+export DB=$TMP/20th_Century_ITS1_mixed.sqlite
+rm -rf $DB
+thapbi_pict load-tax -d $DB -t new_taxdump_2018-12-01
+if [ `sqlite3 $DB "SELECT COUNT(id) FROM taxonomy;"` -ne "595" ]; then echo "Wrong taxonomy count"; false; fi
+thapbi_pict ncbi-import -d $DB tests/ncbi-import/20th_Century_ITS1.fasta
+if [ `sqlite3 $DB "SELECT COUNT(id) FROM data_source;"` -ne "1" ]; then echo "Wrong data_source count"; false; fi
+if [ `sqlite3 $DB "SELECT COUNT(id) FROM its1_source;"` -ne "104" ]; then echo "Wrong its1_source count"; false; fi
+if [ `sqlite3 $DB "SELECT COUNT(id) FROM its1_sequence;"` -ne "87" ]; then echo "Wrong its1_sequence count"; false; fi
+if [ `sqlite3 $DB "SELECT COUNT(id) FROM taxonomy;"` -ne "595" ]; then echo "Wrong taxonomy count"; false; fi
+if [ `thapbi_pict dump -d $DB -f fasta | grep "^>" | grep  "species=Phytophthora " -c` -ne 104 ]; then echo "Wrong Phytophthora species count"; false; fi
+thapbi_pict ncbi-import -d $DB tests/ncbi-import/20th_Century_ITS1_Peronosporaceae.fasta -g
+if [ `sqlite3 $DB "SELECT COUNT(id) FROM data_source;"` -ne "2" ]; then echo "Wrong data_source count"; false; fi
+if [ `sqlite3 $DB "SELECT COUNT(id) FROM its1_source;"` -ne "239" ]; then echo "Wrong its1_source count"; false; fi
+if [ `sqlite3 $DB "SELECT COUNT(id) FROM its1_sequence;"` -ne "95" ]; then echo "Wrong its1_sequence count"; false; fi
+if [ `sqlite3 $DB "SELECT COUNT(id) FROM taxonomy;"` -ne "595" ]; then echo "Wrong taxonomy count"; false; fi
+# 104 + 106 + 29 = 239
+if [ `thapbi_pict dump -d $DB -f fasta | grep "^>" | grep -c "species=Phytophthora "` -ne 104 ]; then echo "Wrong Phytophthora species count";	false; fi
+if [ `thapbi_pict dump -d $DB -f fasta | grep "^>" | grep -c "species=Phytophthora\]"` -n 106 ]; then echo "Wrong Phytophthora genus-only count"; false; fi
+if [ `thapbi_pict dump -d $DB -f fasta | grep "^>" | grep -c -v "Phytophthora"` -ne 29 ]; then echo "Wrong sister genus count"; false; fi
 
 echo "$0 - test_ncbi-import.sh passed"
