dc8e9baa7c7c57321b2790e25998d7bf7ad241d9
peterjc <p.j.a.cock@googlemail.com>
2019-04-26 17:58:40 +0100

    Tests for sample-summary looking at sorting
    In this (contrived) example, when add metadata and sort on
    the description, the order is almost inverted from sorting
    on the filenames.

--- tests/test_sample-summary.sh
+++ tests/test_sample-summary.sh
@@ -26,8 +26,21 @@ for M in identity onebp blast swarmid swarm; do
     rm -rf $TMP/human.txt $TMP/computer.tsv
     thapbi_pict sample-summary -t tests/classify/P-infestans-T30-4.meta.tsv -x 1 -c 2,3,4,5 -m $M -r $TMP/human.txt -o $TMP/computer.tsv tests/classify/
     diff $TMP/human.txt tests/sample-summary/classify-meta.$M.txt
-    # This currently does not include metadata:
+    # This currently does not include metadata, and sorting does not change as only one sample
     diff $TMP/computer.tsv tests/sample-summary/classify.$M.tsv
+done
+
+# More complicated metadata testing
+for M in identity; do
+    rm -rf $TMP/human.txt $TMP/computer.tsv
+    thapbi_pict sample-summary -m $M -r $TMP/human.txt -o $TMP/computer.tsv tests/assess/
+    diff $TMP/human.txt tests/sample-summary/assess.$M.txt
+    diff $TMP/computer.tsv tests/sample-summary/assess.$M.tsv
+    # And again, but with metadata
+    rm -rf $TMP/human.txt $TMP/computer.tsv
+    thapbi_pict sample-summary -m $M -t tests/assess/meta.tsv -x 2 -c 1 -r $TMP/human.txt -o $TMP/computer.tsv tests/assess/
+    diff $TMP/human.txt tests/sample-summary/assess-meta.$M.txt
+    diff $TMP/computer.tsv tests/sample-summary/assess-meta.$M.tsv
 done
 
 echo "$0 - test_sample-summary.sh passed"
