74cfa4b418b7a48a6c3feafc1da4648d7a0c3e5b
peterjc <p.j.a.cock@googlemail.com>
2019-04-30 13:57:09 +0100

    Use tests/prepare-reads/DNAMIX_S95_L001.fasta in tests.
    Goal is to allow all the downstream tests to be run without
    first having to run test_prepare-reads.sh

--- tests/test_plate-summary.sh
+++ tests/test_plate-summary.sh
@@ -11,10 +11,10 @@ thapbi_pict plate-summary -o '' tests/classify 2>&1 | grep "No output file speci
 set -o pipefail
 
 # One method:
-thapbi_pict plate-summary $TMP/DNAMIX_S95_L001.fasta $TMP/DNAMIX_S95_L001.identity.tsv -o $TMP/plate-summary_identity.tsv
+thapbi_pict plate-summary tests/prepare-reads/DNAMIX_S95_L001.fasta $TMP/DNAMIX_S95_L001.identity.tsv -o $TMP/plate-summary_identity.tsv
 
 # Two methods:
-thapbi_pict plate-summary $TMP/DNAMIX_S95_L001.fasta  $TMP/thapbi_swarm/ $TMP/thapbi_blast/ -m blast,swarm -o $TMP/plate-summary_swarm_and_blast.tsv
+thapbi_pict plate-summary tests/prepare-reads/DNAMIX_S95_L001.fasta  $TMP/thapbi_swarm/ $TMP/thapbi_blast/ -m blast,swarm -o $TMP/plate-summary_swarm_and_blast.tsv
 
 # With metadata
 thapbi_pict plate-summary tests/classify -m onebp -o $TMP/plate-summary_onebp.tsv -t tests/classify/P-infestans-T30-4.meta.tsv -x 1 -c 2,3,4,5
