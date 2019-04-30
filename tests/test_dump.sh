1fd2bb2f0944fd296c1c6e42539efec86764c21d
peterjc <p.j.a.cock@googlemail.com>
2019-04-30 15:22:14 +0100

    New test just for thapbi_pict dump (using default DB)

---
+++ tests/test_dump.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+IFS=$'\n\t'
+set -eux
+# Note not using "set -o pipefail" until after check error message with grep
+
+# Note all tests here (initially) using default database:
+
+echo "Checking dump"
+thapbi_pict dump -s fallax 2>&1 | grep "species requires a single genus"
+set -o pipefail
+
+if [ `thapbi_pict dump | grep -c -v "^#"` -ne 15580 ]; then echo "Wrong record count for table dump"; false; fi
+if [ `thapbi_pict dump -f fasta | grep -c "^>"` -ne 15580 ]; then echo "Wrong record count for fasta dump"; false; fi
+
+# With genus filter,
+if [ `thapbi_pict dump -f fasta -g Phytophthora | grep -c "^>"` -ne 13592 ]; then echo "Wrong record count for Phytophthora genus"; false; fi
+
+# With genus and species filter,
+if [ `thapbi_pict dump -f fasta -g Phytophthora -s "fallax, andina" | grep -c "^>"` -ne 7 ]; then echo "Wrong record count for two species"; false; fi
+
+# With genus and clades
+if [ `thapbi_pict dump -f fasta -g Phytophthora -c 8a,8b | grep -c "^>"` -ne 17 ]; then echo "Wrong record count for Phytophthora clade"; false; fi
+
+# With clades only (by DB construction will be Phytophthora only)
+if [ `thapbi_pict dump -c 8a,8b | grep -c -v "^#"` -ne 17 ]; then echo "Wrong record count for clade"; false; fi
+
+echo "$0 - test_dump.sh passed"
