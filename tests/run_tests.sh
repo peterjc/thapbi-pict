b70dd4d1ec553be59b3ef42bd74ad9578749a9be
peterjc <p.j.a.cock@googlemail.com>
2019-04-30 12:37:52 +0100

    Run same actual tests on TravisCI and CircleCI
    Using an if statement to handle missing dependencies.

---
+++ tests/run_tests.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+IFS=$'\n\t'
+set -euo pipefail
+
+python -c "import thapbi_pict; print('Direct import says version ' + thapbi_pict.__version__)"
+thapbi_pict -v
+python -m thapbi_pict -v
+
+tests/test_load-tax.sh
+tests/test_legacy-import.sh
+tests/test_ncbi-import.sh
+
+# Currently can't easily install these on TravisCI
+if ! [ -x "$(command -v flash)" ]; then
+    echo 'WARNING: flash not installed, skipping some tests'
+elif ! [ -x "$(command -v cutadapt)" ]; then
+    echo 'WARNING: cutadapt not installed, skipping some tests'
+else
+    tests/test_prepare-reads.sh
+fi
+
+tests/test_classify.sh
+tests/test_seq-import.sh
+tests/test_assess.sh
+tests/test_sample-summary.sh
+tests/test_plate-summary.sh
