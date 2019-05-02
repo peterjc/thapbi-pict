038fe489eb10194b71cd41de72946aa594c164cb
peterjc <p.j.a.cock@googlemail.com>
2019-05-02 13:31:07 +0100

    Test pipeline on a single sample

--- tests/test_pipeline.sh
+++ tests/test_pipeline.sh
@@ -9,6 +9,21 @@ echo "Checking pipeline"
 thapbi_pict pipeline 2>&1 | grep "the following arguments are required"
 set -o pipefail
 
-# TODO...
+rm -rf $TMP/intermediate $TMP/output
+mkdir $TMP/intermediate $TMP/output
+thapbi_pict pipeline -s $TMP/intermediate -o $TMP/output tests/reads/
+diff $TMP/intermediate/DNAMIX_S95_L001.fasta tests/prepare-reads/DNAMIX_S95_L001.fasta
+diff $TMP/output/sample-summary.txt tests/pipeline/sample-summary.onebp.txt
+diff $TMP/output/sample-summary.tsv tests/pipeline/sample-summary.onebp.tsv
+diff $TMP/output/plate-summary.tsv tests/pipeline/plate-summary.onebp.tsv
+
+# Leaving the intermediate files in place...
+# Run again with some explicit options set (shouldn't change output)
+rm -rf $TMP/output/*
+thapbi_pict pipeline -s $TMP/intermediate -o $TMP/output tests/reads/ -m onebp -a 250
+diff $TMP/intermediate/DNAMIX_S95_L001.fasta tests/prepare-reads/DNAMIX_S95_L001.fasta
+diff $TMP/output/sample-summary.txt tests/pipeline/sample-summary.onebp.txt
+diff $TMP/output/sample-summary.tsv tests/pipeline/sample-summary.onebp.tsv
+diff $TMP/output/plate-summary.tsv tests/pipeline/plate-summary.onebp.tsv
 
 echo "$0 - test_pipeline.sh passed"
