9b7df1d8e9594dfc056104b69c9c40fe48564c0d
peterjc <p.j.a.cock@googlemail.com>
2019-05-02 15:40:21 +0100

    Test with decoy files in the pipeline intermediate folder

--- tests/test_pipeline.sh
+++ tests/test_pipeline.sh
@@ -17,7 +17,12 @@ diff $TMP/output/sample-summary.txt tests/pipeline/sample-summary.onebp.txt
 diff $TMP/output/sample-summary.tsv tests/pipeline/sample-summary.onebp.tsv
 diff $TMP/output/plate-summary.tsv tests/pipeline/plate-summary.onebp.tsv
 
-# Leaving the intermediate files in place...
+# Leaving the intermediate files in place... plus some stray files:
+touch $TMP/intermediate/unwanted.fasta
+touch $TMP/intermediate/unwanted.onebp.tsv
+touch $TMP/intermediate/unwanted.identity.tsv
+touch $TMP/intermediate/distraction.fasta
+touch $TMP/intermediate/ignore-me.onebp.tsv
 # Run again with some explicit options set (shouldn't change output)
 rm -rf $TMP/output/*
 thapbi_pict pipeline -s $TMP/intermediate -o $TMP/output tests/reads/ -m onebp -a 250
