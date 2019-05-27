e67c1982f3d2223b583a8748481430bf59694acf
peterjc <p.j.a.cock@googlemail.com>
2019-05-27 15:16:29 +0100

    Update read-summary for classifier test folder change

--- tests/test_read-summary.sh
+++ tests/test_read-summary.sh
@@ -17,7 +17,7 @@ thapbi_pict read-summary tests/prepare-reads/DNAMIX_S95_L001.fasta $TMP/DNAMIX_S
 thapbi_pict read-summary tests/prepare-reads/DNAMIX_S95_L001.fasta  $TMP/thapbi_swarm/ $TMP/thapbi_blast/ -m blast,swarm -o $TMP/read-summary_swarm_and_blast.tsv
 
 # With metadata, using default method, -m onebp
-thapbi_pict read-summary tests/classify -o $TMP/read-summary_onebp.tsv -t tests/classify/P-infestans-T30-4.meta.tsv -x 1 -c 2,3,4,5 -e $TMP/read-summary_onebp.xlsx
+thapbi_pict read-summary tests/classify/P-infestans-T30-4.fasta tests/classify/P-infestans-T30-4.onebp.tsv -o $TMP/read-summary_onebp.tsv -t tests/classify/P-infestans-T30-4.meta.tsv -x 1 -c 2,3,4,5 -e $TMP/read-summary_onebp.xlsx
 diff $TMP/read-summary_onebp.tsv tests/classify/P-infestans-T30-4.summary.tsv
 
 echo "$0 - test_read-summary.sh passed"
