c2beec76fbd530623de569d7e212dd18b6f915f0
peterjc <p.j.a.cock@googlemail.com>
2019-06-21 17:04:52 +0100

    Test setting pipeline report stem actually works

--- tests/test_pipeline.sh
+++ tests/test_pipeline.sh
@@ -26,11 +26,11 @@ touch $TMP/intermediate/distraction.fasta
 touch $TMP/intermediate/ignore-me.onebp.tsv
 # Run again with some explicit options set (shouldn't change output)
 rm -rf $TMP/output/*
-thapbi_pict pipeline -i tests/reads/ -s $TMP/intermediate -o $TMP/output -m onebp -a 250 -r thapbi-pict
+thapbi_pict pipeline -i tests/reads/ -s $TMP/intermediate -o $TMP/output -m onebp -a 250 -r report
 diff $TMP/intermediate/DNAMIX_S95_L001.fasta tests/prepare-reads/DNAMIX_S95_L001.fasta
-diff $TMP/output/thapbi-pict.samples.txt tests/pipeline/thapbi-pict.samples.onebp.txt
-diff $TMP/output/thapbi-pict.samples.tsv tests/pipeline/thapbi-pict.samples.onebp.tsv
-diff $TMP/output/thapbi-pict.reads.tsv tests/pipeline/thapbi-pict.reads.onebp.tsv
-diff $TMP/output/thapbi-pict.edit-graph.xgmml tests/pipeline/thapbi-pict.edit-graph.xgmml
+diff $TMP/output/report.samples.txt tests/pipeline/thapbi-pict.samples.onebp.txt
+diff $TMP/output/report.samples.tsv tests/pipeline/thapbi-pict.samples.onebp.tsv
+diff $TMP/output/report.reads.tsv tests/pipeline/thapbi-pict.reads.onebp.tsv
+diff $TMP/output/report.edit-graph.xgmml tests/pipeline/thapbi-pict.edit-graph.xgmml
 
 echo "$0 - test_pipeline.sh passed"
