86270e4a8b6930caa432bc8bdae8d165734fce51
peterjc <p.j.a.cock@googlemail.com>
2019-07-01 14:22:28 +0100

    top & bottom prev/next buttons in Sphinx RTD theme
    [ci skip]

--- docs/conf.py
+++ docs/conf.py
@@ -51,6 +51,10 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # Sphinx default was html_theme = "alabaster"
 html_theme = "sphinx_rtd_theme"
 
+# Sphinx Read The Docs theme settings, see
+# https://sphinx-rtd-theme.readthedocs.io/en/latest/configuring.html
+html_theme_options = {"prev_next_buttons_location": "both"}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
