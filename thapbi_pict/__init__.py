435ea25ceceeffa3aa4ff9682eb67366c221d2a0
peterjc <p.j.a.cock@googlemail.com>
2019-07-30 14:42:31 +0100

    Rewrite thapbi_pict/__init__.py module docstring

--- thapbi_pict/__init__.py
+++ thapbi_pict/__init__.py
@@ -6,31 +6,23 @@
 
 """THAPBI *Phytophthora* ITS1 Classifier Tool (PICT).
 
-*Phytophthora* (from Greek meaning plant-destroyer) species are economically
-important plant pathogens, important in both agriculture and forestry. ITS1 is
-short for Internal Transcribed Spacer one, which is a region of eukaryotes
-genomes between the 18S and 5.8S rRNA genes. This is commonly used for
-molecular barcoding, where sequencing this short region can identify species.
-
-This Python package provides ITS1-based diagnostic/profiling tools for the
-Tree Health and Plant Biosecurity Initiative (THAPBI) Phyto-Threats project,
-which is supported by a grant funded jointly by the Biotechnology and
-Biological Sciences Research Council (BBSRC), the Department for Environment,
-Food and Rural affairs (DEFRA), the Economic and Social Research Council
-(ESRC), the Forestry Commission, and the Natural Environment Research Council
-(NERC) and the Scottish Government, under the Tree Health and Plant
-Biosecurity Initiative (THAPBI).
-
-The `tool documentation <https://thapbi-pict.readthedocs.io/>`_ is
-hosted by `Read The Docs <https://readthedocs.org/>`_, generated
-automatically from the ``docs/`` folder of the `software repository
-<https://github.com/peterjc/thapbi-pict/>`_.
-
 You would typically use THAPBI PICT via the command line tool it defines::
 
     $ thapbi_pict --help
     ...
 
+However, it is also possible to call functions etc from within Python. The
+top level package currently only defines the tool version:
+
+    >>> from thapbi_pict import __version__
+    >>> print(__version__)
+    ...
+
+The `tool documentation <https://thapbi-pict.readthedocs.io/>`_ is
+hosted by `Read The Docs <https://readthedocs.org/>`_, generated
+automatically from the ``docs/`` folder of the `software repository
+<https://github.com/peterjc/thapbi-pict/>`_ and the *"docstrings"*
+within the source code which document the Python API.
 """
 
 __version__ = "0.3.6"
