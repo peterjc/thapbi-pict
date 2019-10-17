9b43fef78c0730d89ef5eff14eec96cb65c5e5a6
peterjc <p.j.a.cock@googlemail.com>
2019-10-17 10:58:35 +0100

    Run this in verbose/debug mode
    Useful to see which of our curated entries fail species validation:
    
    WARNING: Could not validate species 'Phytophthora sp. aff. meadii' from '2a_Phytophthora_sp._aff._meadii_KC875839'
    WARNING: Could not validate species 'Phytophthora sp. mekongensis type3' from '2a_P._sp._mekongensis_type3_KM501564'
    WARNING: Could not validate species 'Phytophthora taxon kunnunara' from '9_Phytophthora_taxon_kunnunara_EF437222'
    WARNING: Could not validate species 'Phytophthora sp Pecan' from '4_Phytophthora_sp_Pecan_GU997621'
    WARNING: Could not validate species 'Phytophthora sp. mekongensis type1' from '2a_Phytophthora_sp._mekongensis_type1_KC875838'
    WARNING: Could not validate species 'Phytophthora taxon cuyabensis' from '9_Phytophthora_taxon_cuyabensis_FJ801995'
    WARNING: Could not validate species 'Phytophthora taxon umtamvuna' from '9_Phytophthora_taxon_umtamvuna_KC855181'
    WARNING: Could not validate species 'Phytophthora sp. personii' from '6_Phytophthora_sp._personii_EU301169'
    WARNING: Could not validate species 'Phytophthora taxon aquatilis' from '2c_Phytophthora_taxon_aquatilis_FJ666126'
    WARNING: Could not validate species 'Phytophthora taxon paludosa' from '6_Phytophthora_taxon_paludosa_HQ012953'
    WARNING: Could not validate species 'Phytophthora x stagnum' from '6b_Phytophthora_X_stagnum_KJ705084'
    WARNING: Could not validate species 'Phytophthora caryae' from '2_Phytophthora_caryae_KC122237.1'
    
    (plus the synthetic constructs)

--- database/build_CURATED.sh
+++ database/build_CURATED.sh
@@ -9,7 +9,7 @@ rm -rf "$DB.sqlite" "$DB.fasta" "$DB.txt" "$DB.sql"
 
 thapbi_pict load-tax -d "$DB.sqlite" -t "$TAX"
 # In strict mode this will ignore the synthetic controls, we add them later:
-thapbi_pict legacy-import -d "$DB.sqlite" -i "$CURATED"
+thapbi_pict legacy-import -d "$DB.sqlite" -i "$CURATED" -v
 
 # Ad-hoc fix for NCBI taxonomy not yet having caught up with community consensus.
 # At the 7th Meeting of the International Union of Forest Research Organisations
