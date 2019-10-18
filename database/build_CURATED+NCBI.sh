3bd146e8dd6c58278bdc0d0b40d98a307f711883
peterjc <p.j.a.cock@googlemail.com>
2019-10-18 14:32:24 +0100

    Use the cleaned up curated FASTA file

--- database/build_CURATED+NCBI.sh
+++ database/build_CURATED+NCBI.sh
@@ -2,14 +2,13 @@
 VERSION=`thapbi_pict -v | sed "s/THAPBI PICT //g"`
 echo "Using THAPBI PICT $VERSION"
 set -euo pipefail
-CURATED=legacy/Phytophthora_ITS_database_v0.006.fasta
+CURATED=Phytophthora_ITS1_curated.fasta
 TAX=new_taxdump_2019-09-01
 DB=CURATED+NCBI
 rm -rf "$DB.sqlite" "$DB.fasta" "$DB.txt" "$DB.sql"
 
 thapbi_pict load-tax -d "$DB.sqlite" -t "$TAX"
-# In strict mode this will ignore the synthetic controls, we add them later:
-thapbi_pict legacy-import -d "$DB.sqlite" -i "$CURATED"
+thapbi_pict ncbi-import -d "$DB.sqlite" -i "$CURATED" --left '' --right ''
 thapbi_pict ncbi-import -d "$DB.sqlite" -i 2019-04-03-ITS_Peronosporales_16394.fasta -g
 
 # Ad-hoc fix for NCBI taxonomy not yet having caught up with community consensus.
