d57cd6ffe79454d48395fa1e77d2d7d66193fa48
peterjc <p.j.a.cock@googlemail.com>
2019-11-22 17:33:03 +0000

    Removed a couple of entries

--- tests/test_dump.sh
+++ tests/test_dump.sh
@@ -20,14 +20,14 @@ thapbi_pict dump -s fallax 2>&1 | grep "species requires a single genus"
 set -o pipefail
 
 if [ `thapbi_pict dump | grep -c -v "^#"` -ne 14966 ]; then echo "Wrong source count for table dump"; false; fi
-if [ `thapbi_pict dump -f fasta | grep -c "^>"` -ne 3960 ]; then echo "Wrong source count for fasta dump"; false; fi
+if [ `thapbi_pict dump -f fasta | grep -c "^>"` -ne 3958 ]; then echo "Wrong source count for fasta dump"; false; fi
 
-if [ `thapbi_pict dump --minimal | grep -c -v "^#"` -ne 3960 ]; then echo "Wrong sequence count for minimal table dump"; false; fi
-if [ `thapbi_pict dump -m -f fasta | grep -c "^>"` -ne 3960 ]; then echo "Wrong sequence count for minimal fasta dump"; false; fi
+if [ `thapbi_pict dump --minimal | grep -c -v "^#"` -ne 3958 ]; then echo "Wrong sequence count for minimal table dump"; false; fi
+if [ `thapbi_pict dump -m -f fasta | grep -c "^>"` -ne 3958 ]; then echo "Wrong sequence count for minimal fasta dump"; false; fi
 
 # With genus filter,
-if [ `thapbi_pict dump -f txt -g Phytophthora | grep -v -c "^#"` -ne 13048 ]; then echo "Wrong source for Phytophthora genus"; false; fi
-if [ `thapbi_pict dump -f fasta -g Phytophthora -m | grep -c "^>"` -ne 3272 ]; then echo "Wrong sequence for Phytophthora genus"; false; fi
+if [ `thapbi_pict dump -f txt -g Phytophthora | grep -v -c "^#"` -ne 13049 ]; then echo "Wrong source for Phytophthora genus"; false; fi
+if [ `thapbi_pict dump -f fasta -g Phytophthora -m | grep -c "^>"` -ne 3271 ]; then echo "Wrong sequence for Phytophthora genus"; false; fi
 
 # With genus and species filter,
 if [ `thapbi_pict dump -f txt -g Phytophthora -s "fallax, andina" | grep -v -c "^#"` -ne 7 ]; then echo "Wrong source for two species"; false; fi
