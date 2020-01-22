23883d5d13a7cb66551a4ac6339df6333adbfffb
peterjc <p.j.a.cock@googlemail.com>
2020-01-22 13:29:38 +0000

    Redekar_et_al_2019_sup_table_3.fasta in curated-import test

--- tests/test_curated-import.sh
+++ tests/test_curated-import.sh
@@ -49,5 +49,14 @@ if [ `sqlite3 $DB "SELECT COUNT(id) FROM its1_sequence;"` -ne "2" ]; then echo "
 if [ `sqlite3 $DB "SELECT COUNT(id) FROM taxonomy;"` -ne "8" ]; then echo "Wrong taxonomy count"; false; fi
 if [ `sqlite3 $DB "SELECT DISTINCT genus, species FROM taxonomy;" | wc -l` -ne 8 ]; then echo "Wrong species count"; false; fi
 
+export DB=$TMP/Redekar_et_al_2019_sup_table_3.sqlite
+rm -rf $DB
+# This uses the default semi-colon separator
+thapbi_pict curated-import -x -d $DB -i tests/recycled_water/Redekar_et_al_2019_sup_table_3.fasta
+if [ `sqlite3 $DB "SELECT COUNT(id) FROM data_source;"` -ne "1" ]; then echo "Wrong data_source count"; false; fi
+if [ `sqlite3 $DB "SELECT COUNT(id) FROM its1_source;"` -ne "1451" ]; then echo "Wrong its1_source count"; false; fi
+if [ `sqlite3 $DB "SELECT COUNT(id) FROM its1_sequence;"` -ne "838" ]; then echo "Wrong its1_sequence count"; false; fi
+if [ `sqlite3 $DB "SELECT COUNT(id) FROM taxonomy;"` -ne "430" ]; then echo "Wrong taxonomy count"; false; fi
+if [ `sqlite3 $DB "SELECT DISTINCT genus, species FROM taxonomy;" | wc -l` -ne 430 ]; then echo "Wrong species count"; false; fi
 
 echo "$0 - test_curated-import.sh passed"
