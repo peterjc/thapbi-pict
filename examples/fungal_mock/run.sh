e56d71a52a9c51907b80f914fd4a98c875c936a0
Peter Cock <p.j.a.cock@googlemail.com>
2020-02-04 16:13:57 +0000

    Run fungal mock communities with abundance threshold 10.
    This simplifies my planned discussion as it lets us mostly
    answer questions like was sequence XXX there at all?
    
    Using minimum of 100 (default) gives under 800 unique over samples.
    
    Using 10 gives 8.8k unique (with minimal run time change, about 6 mins).
    
    Using 2 gives 75k unique.
    
    No threshold (-a 0 or -a 1) gives 450k total unique entries over samples
    (and took about four times longer).

--- examples/fungal_mock/run.sh
+++ examples/fungal_mock/run.sh
@@ -7,11 +7,14 @@ function analyse {
     rm -rf ${NAME}.sqlite
     thapbi_pict curated-import -i ${MARKER}.fasta -d ${NAME}.sqlite --left $LEFT --right $RIGHT -x
 
-    echo "Running analysis with default abundance threshold"
+    echo "Running analysis with minimum abundance threshold ten"
+    # No threshold (-a 0 or -a 1) gives 450k total unique entries over samples
+    # Using minimum of 2 gives 75k unique, minimum of 10 gives 8.8k unique.
+    # Using minimum of 100 (default) gives under 800 unique over samples.
     mkdir -p $LIBRARY/$NAME
     thapbi_pict pipeline -d ${NAME}.sqlite --left $LEFT --right $RIGHT \
 		-i $LIBRARY/raw_data/ $LIBRARY/expected/ \
-		-s $LIBRARY/$NAME -o $LIBRARY/ -r $NAME \
+		-s $LIBRARY/$NAME -o $LIBRARY/ -r $NAME -a 10 \
 		-t $LIBRARY/metadata.tsv -c 5,6,7,3,4,2 -x 1 -g 6
     echo "$NAME done"
 }
