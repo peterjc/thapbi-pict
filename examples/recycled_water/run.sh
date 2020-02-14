c3e6db53a501f28cddfca06a4007e50eba7b1197
Peter Cock <p.j.a.cock@googlemail.com>
2020-02-14 14:47:03 +0000

    Basic recycled_water/run.sh doing core of worked example
    This deliberately does not do all the exploratory bits
    like running with the defaults, or alternative ways to
    build the custom database.

---
+++ examples/recycled_water/run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -eup pipeline
+
+echo =====================
+echo Recycled water - ITS1
+echo =====================
+
+echo "Building ITS1 database"
+# Remove any pre-existing DB
+rm -rf Redekar_et_al_2019_sup_table_3.sqlite
+
+# Loading NCBI taxonomy for handling synonyms
+thapbi_pict load-tax -d Redekar_et_al_2019_sup_table_3.sqlite -t taxdmp_2019-12-01/
+
+# Using -x / --lax (does not insist on taxonomy match)
+# Not giving primers, sequences are already trimmed
+thapbi_pict curated-import -x \
+	    -d Redekar_et_al_2019_sup_table_3.sqlite \
+	    -i Redekar_et_al_2019_sup_table_3.fasta
+
+echo "Drawing edit-graph for database entries alone"
+# Using -s / --showdb
+thapbi_pict edit-graph -s \
+	    -d Redekar_et_al_2019_sup_table_3.sqlite \
+	    -o Redekar_et_al_2019_sup_table_3.xgmml
+
+echo "Running analysis"
+mkdir -p intermediate/ summary/
+thapbi_pict pipeline -i raw_data/ -s intermediate/ -o summary/ \
+	    --left GAAGGTGAAGTCGTAACAAGGTTTCCGTAGGTGAACCTGCGGAAGGATCATTA \
+	    --right AGCGTTCTTCATCGATGTGC \
+	    -d Redekar_et_al_2019_sup_table_3.sqlite -m onebp \
+	    -r recycled-water-custom -t metadata.tsv -c 1,2,3,4,5,6,7
