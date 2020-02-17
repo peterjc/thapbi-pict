8e841e8005dd39a1639ce4606096a5ec8843e0ac
Peter Cock <p.j.a.cock@googlemail.com>
2020-02-17 11:27:37 +0000

    examples/woody_hosts/run.sh doing pipeline only

---
+++ examples/woody_hosts/run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -eup pipeline
+
+echo NOTE: Expected first time run time is under 5 minues,
+echo repeat runs about 1 minute just to regenerate reports.
+echo
+echo ==================
+echo Woody hosts - ITS1
+echo ==================
+
+echo "Running analysis"
+mkdir -p intermediate/ summary/
+
+echo "Pipeline without metadata..."
+# Using default report naming
+thapbi_pict pipeline -i raw_data/ -s intermediate/ -o summary/ \
+	    -n raw_data/NEGATIVE*.fastq.gz
+
+echo "Pipeline with metadata..."
+# Reuses the intermediate files (prepared FASTA and classifer output)
+# Giving report name stem (so not to over-write reports without metadata)
+thapbi_pict pipeline -i raw_data/ -s intermediate/ -o summary/ \
+	    -n raw_data/NEGATIVE*.fastq.gz -r with-metadata \
+	    -t metadata.tsv -c 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 -x 16 -f 20
