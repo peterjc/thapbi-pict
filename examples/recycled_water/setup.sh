6695916ec1eea8808f020fee03392fddee0fd368
Peter Cock <p.j.a.cock@googlemail.com>
2020-02-20 12:05:52 +0000

    Need to update setup.sh after changing column order

--- examples/recycled_water/setup.sh
+++ examples/recycled_water/setup.sh
@@ -16,7 +16,7 @@ if [ ! -f raw_data/MD5SUM.txt ]; then
     echo "ERROR: Missing raw_data/MD5SUM.txt"
     false
 fi
-for ACC in `grep ^SRR metadata.tsv | cut -f 1`; do
+for ACC in `grep ^SRR PRJNA417859.txt| cut -f 1`; do
     # echo "Downloading $ACC"
     # Column 6 should have two URLs (R1 and R2), semi-colon separated:
     for URL in `grep ^$ACC PRJNA417859.txt | cut -f 6 | sed "s/;/ /g"`; do
