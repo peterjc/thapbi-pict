6c4770574137892c5048f074ae9296040c9bf84c
Peter Cock <p.j.a.cock@googlemail.com>
2020-03-10 14:49:19 +0000

    Comment about recycled_water with default pipeline

--- examples/recycled_water/run.sh
+++ examples/recycled_water/run.sh
@@ -8,6 +8,14 @@ echo =====================
 echo Recycled water - ITS1
 echo =====================
 
+# Following the documentation would first run the pipeline with
+# default settings for primer and database:
+#
+# mkdir intermediate_defaults/ summary_defaults/
+# thapbi_pict pipeline \
+#             -i raw_data/ -o summary_defaults/ -s intermediate_defaults/ \
+#             -r recycled-water-defaults -t metadata.tsv -x 7 -c 1,2,3,4,5,6
+
 echo "Building ITS1 database"
 # Remove any pre-existing DB
 rm -rf Redekar_et_al_2019_sup_table_3.sqlite
