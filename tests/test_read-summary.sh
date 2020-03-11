3a6646eea7b36279f4dc0fd673edd5a4bc20ef2c
Peter Cock <p.j.a.cock@googlemail.com>
2020-03-11 17:22:29 +0000

    Update tests
    Had to drop some of the sample-summary tests using
    the tests/assess/ folder as these lack matching
    FASTA files (now required).

--- tests/test_read-summary.sh
+++ tests/test_read-summary.sh
@@ -16,22 +16,20 @@ echo "====================="
 echo "Checking read-summary"
 echo "====================="
 set -x
-thapbi_pict read-summary 2>&1 | grep "the following arguments are required"
-thapbi_pict read-summary -o '' -i tests/classify 2>&1 | grep "Output directory name blank"
+thapbi_pict summary 2>&1 | grep "the following arguments are required"
+thapbi_pict summary -o '' -i tests/classify 2>&1 | grep "Output directory name blank"
 set -o pipefail
 
-# One method:
-thapbi_pict read-summary -i tests/prepare-reads/DNAMIX_S95_L001.fasta $TMP/DNAMIX_S95_L001.identity.tsv -m identity -o $TMP/ -r summary
-
-# Two methods:
-thapbi_pict read-summary -i tests/prepare-reads/DNAMIX_S95_L001.fasta  $TMP/thapbi_swarm/ $TMP/thapbi_blast/ -m blast,swarm -o $TMP/ -r summary_swarm_and_blast
+thapbi_pict summary -i tests/prepare-reads/DNAMIX_S95_L001.fasta \
+	    $TMP/DNAMIX_S95_L001.identity.tsv \
+	    -m identity -o $TMP/ -r summary
 
 # With metadata, using default method, -m onebp
-thapbi_pict read-summary --input tests/classify/P-infestans-T30-4.fasta tests/classify/P-infestans-T30-4.onebp.tsv -o $TMP/ -r summary -t tests/classify/P-infestans-T30-4.meta.tsv -x 1 -c 2,3,4,5
+thapbi_pict summary --input tests/classify/P-infestans-T30-4.fasta tests/classify/P-infestans-T30-4.onebp.tsv -o $TMP/ -r summary -t tests/classify/P-infestans-T30-4.meta.tsv -x 1 -c 2,3,4,5
 diff $TMP/summary.reads.onebp.tsv tests/classify/P-infestans-T30-4.summary.tsv
 
 # Now require metadata, but give entire folder as input
-thapbi_pict read-summary --input tests/classify/ -o $TMP/ -r summary -t tests/classify/P-infestans-T30-4.meta.tsv -x 1 -c 2,3,4,5 -r summary -q
+thapbi_pict summary --input tests/classify/ -o $TMP/ -r summary -t tests/classify/P-infestans-T30-4.meta.tsv -x 1 -c 2,3,4,5 -r summary -q
 diff $TMP/summary.reads.onebp.tsv tests/classify/P-infestans-T30-4.summary.tsv
 
 
--- tests/test_sample-summary.sh
+++ tests/test_sample-summary.sh
@@ -16,51 +16,33 @@ echo "======================="
 echo "Checking sample-summary"
 echo "======================="
 set -x
-thapbi_pict sample-summary 2>&1 | grep "the following arguments are required"
-thapbi_pict sample-summary -o '' -i tests/classify 2>&1 | grep "Output directory name blank"
+thapbi_pict summary 2>&1 | grep "the following arguments are required"
+thapbi_pict summary -o '' -i tests/classify 2>&1 | grep "Output directory name blank"
 set -o pipefail
 
 # Passing filename, default method, explicit min abundance
 rm -rf $TMP/human.txt $TMP/test-case.tsv $TMP/test-case.xlsx
-thapbi_pict sample-summary -m identity -a 99 -o $TMP/ -r test-case -i tests/classify/*.identity.tsv
+thapbi_pict summary -m identity -a 99 -o $TMP/ -r test-case \
+	    -i tests/classify/*.fasta tests/classify/*.identity.tsv
 diff $TMP/test-case.samples.identity.txt tests/sample-summary/classify.identity.txt
 diff $TMP/test-case.samples.identity.tsv tests/sample-summary/classify.identity.tsv
 
 # Passing a folder, trying different methods
-# Skipping swarm based classifiers and they don't work on one of the test cases
-for M in identity onebp blast swarmid swarm; do
+for M in identity onebp blast; do
     rm -rf $TMP/test-case.samples.$M.txt $TMP/test-case.samples.$M.tsv
-    thapbi_pict sample-summary -m $M -r test-case --output $TMP/ --input tests/classify/
+    thapbi_pict summary -m $M -r test-case --output $TMP/ --input tests/classify/
     diff $TMP/test-case.samples.$M.txt tests/sample-summary/classify.$M.txt
     diff $TMP/test-case.samples.$M.tsv tests/sample-summary/classify.$M.tsv
     # And again, but with metadata
     rm -rf $TMP/test-case.samples.$M.txt $TMP/test-case.samples.$M.tsv
-    thapbi_pict sample-summary -t tests/classify/P-infestans-T30-4.meta.tsv -x 1 -c 2,3,4,5 -m $M -o $TMP -r test-case -i tests/classify/
+    thapbi_pict summary -t tests/classify/P-infestans-T30-4.meta.tsv -x 1 -c 2,3,4,5 -m $M -o $TMP -r test-case -i tests/classify/
     diff $TMP/test-case.samples.$M.txt tests/sample-summary/classify-meta.$M.txt
     diff $TMP/test-case.samples.$M.tsv tests/sample-summary/classify-meta.$M.tsv
     # Now require metadata...
     rm -rf $TMP/test-case.samples.$M.txt $TMP/test-case.samples.$M.tsv
-    thapbi_pict sample-summary -t tests/classify/P-infestans-T30-4.meta.tsv -x 1 -c 2,3,4,5 -m $M -o $TMP/ -r test-case -i tests/classify/ -q
+    thapbi_pict summary -t tests/classify/P-infestans-T30-4.meta.tsv -x 1 -c 2,3,4,5 -m $M -o $TMP/ -r test-case -i tests/classify/ -q
     diff $TMP/test-case.samples.$M.txt tests/sample-summary/classify-meta-req.$M.txt
     diff $TMP/test-case.samples.$M.tsv tests/sample-summary/classify-meta-req.$M.tsv
-done
-
-# More complicated metadata testing
-for M in identity; do
-    rm -rf $TMP/test-case.samples.$M.txt $TMP/test-case.samples.$M.tsv $TMP/test-case.xlsx
-    thapbi_pict sample-summary -m $M -o $TMP/ -r test-case -i tests/assess/
-    diff $TMP/test-case.samples.$M.txt tests/sample-summary/assess.$M.txt
-    diff $TMP/test-case.samples.$M.tsv tests/sample-summary/assess.$M.tsv
-    # And again, but with metadata
-    rm -rf $TMP/test-case.samples.$M.txt $TMP/test-case.samples.$M.tsv $TMP/test-case.xlsx
-    thapbi_pict sample-summary -m $M -t tests/assess/meta.tsv -x 2 -c 1 -o $TMP/ -r test-case -i tests/assess/
-    diff $TMP/test-case.samples.$M.txt tests/sample-summary/assess-meta.$M.txt
-    diff $TMP/test-case.samples.$M.tsv tests/sample-summary/assess-meta.$M.tsv
-    # And now requiring the metadata (no change):
-    rm -rf $TMP/test-case.samples.$M.txt $TMP/test-case.samples.$M.tsv $TMP/test-case.xlsx
-    thapbi_pict sample-summary -m $M -t tests/assess/meta.tsv -x 2 -c 1 -o $TMP/ -r test-case -i tests/assess/ -q
-    diff $TMP/test-case.samples.$M.txt tests/sample-summary/assess-meta.$M.txt
-    diff $TMP/test-case.samples.$M.tsv tests/sample-summary/assess-meta.$M.tsv
 done
 
 echo "$0 - test_sample-summary.sh passed"
