c0f3a1adfe390284235d4920aed8b44363e3a47e
Peter Cock <p.j.a.cock@googlemail.com>
2020-03-11 15:50:24 +0000

    Change summary command line API to match pipeline

--- tests/test_read-summary.sh
+++ tests/test_read-summary.sh
@@ -17,22 +17,22 @@ echo "Checking read-summary"
 echo "====================="
 set -x
 thapbi_pict read-summary 2>&1 | grep "the following arguments are required"
-thapbi_pict read-summary -o '' -i tests/classify 2>&1 | grep "No output file specified"
+thapbi_pict read-summary -o '' -i tests/classify 2>&1 | grep "Output directory does not exist"
 set -o pipefail
 
 # One method:
-thapbi_pict read-summary -i tests/prepare-reads/DNAMIX_S95_L001.fasta $TMP/DNAMIX_S95_L001.identity.tsv -m identity -o $TMP/read-summary_identity.tsv -e $TMP/read-summary_swarm_and_blast.xlsx
+thapbi_pict read-summary -i tests/prepare-reads/DNAMIX_S95_L001.fasta $TMP/DNAMIX_S95_L001.identity.tsv -m identity -o $TMP/ -r summary
 
 # Two methods:
-thapbi_pict read-summary -i tests/prepare-reads/DNAMIX_S95_L001.fasta  $TMP/thapbi_swarm/ $TMP/thapbi_blast/ -m blast,swarm -o $TMP/read-summary_swarm_and_blast.tsv
+thapbi_pict read-summary -i tests/prepare-reads/DNAMIX_S95_L001.fasta  $TMP/thapbi_swarm/ $TMP/thapbi_blast/ -m blast,swarm -o $TMP/ -r summary_swarm_and_blast
 
 # With metadata, using default method, -m onebp
-thapbi_pict read-summary --input tests/classify/P-infestans-T30-4.fasta tests/classify/P-infestans-T30-4.onebp.tsv -o $TMP/read-summary_onebp.tsv -t tests/classify/P-infestans-T30-4.meta.tsv -x 1 -c 2,3,4,5 -e $TMP/read-summary_onebp.xlsx
-diff $TMP/read-summary_onebp.tsv tests/classify/P-infestans-T30-4.summary.tsv
+thapbi_pict read-summary --input tests/classify/P-infestans-T30-4.fasta tests/classify/P-infestans-T30-4.onebp.tsv -o $TMP/ -r summary -t tests/classify/P-infestans-T30-4.meta.tsv -x 1 -c 2,3,4,5
+diff $TMP/summary.reads.onebp.tsv tests/classify/P-infestans-T30-4.summary.tsv
 
 # Now require metadata, but give entire folder as input
-thapbi_pict read-summary --input tests/classify/ -o $TMP/read-summary_onebp.tsv -t tests/classify/P-infestans-T30-4.meta.tsv -x 1 -c 2,3,4,5 -e $TMP/read-summary_onebp.xlsx -q
-diff $TMP/read-summary_onebp.tsv tests/classify/P-infestans-T30-4.summary.tsv
+thapbi_pict read-summary --input tests/classify/ -o $TMP/ -r summary -t tests/classify/P-infestans-T30-4.meta.tsv -x 1 -c 2,3,4,5 -r summary -q
+diff $TMP/summary.reads.onebp.tsv tests/classify/P-infestans-T30-4.summary.tsv
 
 
 echo "$0 - test_read-summary.sh passed"
