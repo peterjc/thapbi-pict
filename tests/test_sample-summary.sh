f9f63aa893f1b1289b23ad5cc58a9e19e1de79d7
Peter Cock <p.j.a.cock@googlemail.com>
2020-03-12 08:53:46 +0000

    Rename test folder to match command's new name

--- tests/test_sample-summary.sh
+++ tests/test_sample-summary.sh
@@ -12,9 +12,9 @@ set -eu
 
 export TMP=${TMP:-/tmp}
 
-echo "======================="
-echo "Checking sample-summary"
-echo "======================="
+echo "================"
+echo "Checking summary"
+echo "================"
 set -x
 thapbi_pict summary 2>&1 | grep "the following arguments are required"
 thapbi_pict summary -o '' -i tests/classify 2>&1 | grep "Output directory name blank"
@@ -24,25 +24,25 @@ set -o pipefail
 rm -rf $TMP/human.txt $TMP/test-case.tsv $TMP/test-case.xlsx
 thapbi_pict summary -m identity -a 99 -o $TMP/ -r test-case \
 	    -i tests/classify/*.fasta tests/classify/*.identity.tsv
-diff $TMP/test-case.samples.identity.txt tests/sample-summary/classify.identity.txt
-diff $TMP/test-case.samples.identity.tsv tests/sample-summary/classify.identity.tsv
+diff $TMP/test-case.samples.identity.txt tests/summary/classify.identity.txt
+diff $TMP/test-case.samples.identity.tsv tests/summary/classify.identity.tsv
 
 # Passing a folder, trying different methods
 for M in identity onebp blast; do
     rm -rf $TMP/test-case.samples.$M.txt $TMP/test-case.samples.$M.tsv
     thapbi_pict summary -m $M -r test-case --output $TMP/ --input tests/classify/
-    diff $TMP/test-case.samples.$M.txt tests/sample-summary/classify.$M.txt
-    diff $TMP/test-case.samples.$M.tsv tests/sample-summary/classify.$M.tsv
+    diff $TMP/test-case.samples.$M.txt tests/summary/classify.$M.txt
+    diff $TMP/test-case.samples.$M.tsv tests/summary/classify.$M.tsv
     # And again, but with metadata
     rm -rf $TMP/test-case.samples.$M.txt $TMP/test-case.samples.$M.tsv
     thapbi_pict summary -t tests/classify/P-infestans-T30-4.meta.tsv -x 1 -c 2,3,4,5 -m $M -o $TMP -r test-case -i tests/classify/
-    diff $TMP/test-case.samples.$M.txt tests/sample-summary/classify-meta.$M.txt
-    diff $TMP/test-case.samples.$M.tsv tests/sample-summary/classify-meta.$M.tsv
+    diff $TMP/test-case.samples.$M.txt tests/summary/classify-meta.$M.txt
+    diff $TMP/test-case.samples.$M.tsv tests/summary/classify-meta.$M.tsv
     # Now require metadata...
     rm -rf $TMP/test-case.samples.$M.txt $TMP/test-case.samples.$M.tsv
     thapbi_pict summary -t tests/classify/P-infestans-T30-4.meta.tsv -x 1 -c 2,3,4,5 -m $M -o $TMP/ -r test-case -i tests/classify/ -q
-    diff $TMP/test-case.samples.$M.txt tests/sample-summary/classify-meta-req.$M.txt
-    diff $TMP/test-case.samples.$M.tsv tests/sample-summary/classify-meta-req.$M.tsv
+    diff $TMP/test-case.samples.$M.txt tests/summary/classify-meta-req.$M.txt
+    diff $TMP/test-case.samples.$M.tsv tests/summary/classify-meta-req.$M.tsv
 done
 
 echo "$0 - test_sample-summary.sh passed"
