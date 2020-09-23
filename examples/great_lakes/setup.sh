cc8b7d8de30a3707ac99d1add52ef66428a3b65d
Peter Cock <p.j.a.cock@googlemail.com>
2020-09-23 22:57:41 +0100

    Setup expected values for mocks/controls

--- examples/great_lakes/setup.sh
+++ examples/great_lakes/setup.sh
@@ -7,6 +7,43 @@ if [ ! -f raw_data/MD5SUM.txt ]; then
     echo "ERROR: Missing raw_data/MD5SUM.txt"
     false
 fi
+for NAME in MOL16S SPH16S; do
+    mkdir -p expected/$NAME/
+    for ACC in `grep Blank PRJNA379165.txt | grep $NAME | cut -f 1`; do
+        FILE=expected/$NAME/$ACC.known.tsv
+        if [ -f $FILE ]; then
+            echo "Already have $FILE"
+        else
+            echo "Linking $FILE to negative control"
+            cd expected/$NAME/
+            ln -s ../negative_control.known.tsv $ACC.known.tsv
+            cd ../..
+        fi
+    done
+    for ACC in `grep Mock PRJNA379165.txt | grep -v $NAME | cut -f 1`; do
+        FILE=expected/$NAME/$ACC.known.tsv
+        if [ -f $FILE ]; then
+            echo "Already have $FILE"
+        else
+            echo "Linking $FILE to negative control"
+            cd expected/$NAME/
+            ln -s ../negative_control.known.tsv $ACC.known.tsv
+            cd ../..
+        fi
+    done
+    for ACC in `grep Mock PRJNA379165.txt | grep $NAME | cut -f 1`; do
+        FILE=expected/$NAME/$ACC.known.tsv
+        if [ -f $FILE ]; then
+            echo "Already have $FILE"
+        else
+            echo "Linking $FILE to mock community"
+            cd expected/$NAME/
+            ln -s ../mock_community.$NAME.known.tsv $ACC.known.tsv
+            cd ../..
+        fi
+    done
+done
+
 for ACC in `grep ^SRR PRJNA379165.txt| cut -f 1`; do
     # echo "Downloading $ACC"
     # Column 7 should have two URLs (R1 and R2), semi-colon separated:
