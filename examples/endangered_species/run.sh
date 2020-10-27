9cb0e3a563e6d7c19b82434da16dda8438263059
Peter Cock <p.j.a.cock@googlemail.com>
2020-10-27 23:39:43 +0000

    Only want a single FASTA header when pool
    Old version 'worked' but extra headers got read as
    part of the precededing FASTA sequence and appeared
    in the read level report.

--- examples/endangered_species/run.sh
+++ examples/endangered_species/run.sh
@@ -34,11 +34,10 @@ function pool {
     rm -rf intermediate_pool/*
     echo "Pooling intermediate FASTA files..."
     # Excluding primer specific header lines with grep,
-    # duplicate lines for raw fastq count etc harmless.
+    # only want a single header
     for S in `cut -f 4 PRJEB18620.txt | grep -v "sample_alias"`; do
-        cat intermediate/*/$S.fasta \
-            | grep -v -E "(_primer|cutadapt|abundance)" \
-            > intermediate_pool/$S.fasta
+        grep "^#" intermediate/16S/$S.fasta | grep -v -E "(_primer|cutadapt|abundance)" > intermediate_pool/$S.fasta
+        cat intermediate/*/$S.fasta | grep -v "^#" >> intermediate_pool/$S.fasta
     done
 
     echo "Computing species list for combined header..."
