78f1dc6c975e0c726567dbeb97c2ab19ffb069a8
Peter Cock <p.j.a.cock@googlemail.com>
2021-01-26 10:11:50 +0000

    Update copyright to 2020; missing license header

--- thapbi_pict/__init__.py
+++ thapbi_pict/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2018-2019 by Peter Cock, The James Hutton Institute.
+# Copyright 2018-2020 by Peter Cock, The James Hutton Institute.
 # All rights reserved.
 # This file is part of the THAPBI Phytophthora ITS1 Classifier Tool (PICT),
 # and is released under the "MIT License Agreement". Please see the LICENSE
--- thapbi_pict/conflicts.py
+++ thapbi_pict/conflicts.py
@@ -1,3 +1,8 @@
+# Copyright 2019-2020 by Peter Cock, The James Hutton Institute.
+# All rights reserved.
+# This file is part of the THAPBI Phytophthora ITS1 Classifier Tool (PICT),
+# and is released under the "MIT License Agreement". Please see the LICENSE
+# file that should have been included as part of this package.
 """Explore conflicts at species and genus level."""
 import sys
 
