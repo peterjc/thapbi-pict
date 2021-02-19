e3cfa4c2d7f924b10635e0418e4d5ae28d614a31
Peter Cock <p.j.a.cock@googlemail.com>
2021-02-19 12:25:01 +0000

    Only use 2 taxonomy dumps in the tests
    Briefly had three with the addition of test_build_db.sh
    but this now settles on one old style (~50MB) and one
    new style (~100MB).

--- tests/test_load-tax.sh
+++ tests/test_load-tax.sh
@@ -19,30 +19,32 @@ set -x
 thapbi_pict load-tax 2>&1 | grep "the following arguments are required"
 set -o pipefail
 
+# Same taxonomy as test_ncbi-import.sh (new style, ~100MB)
 if [ ! -f "new_taxdump_2019-09-01.zip" ]; then curl -L -O "https://ftp.ncbi.nih.gov/pub/taxonomy/taxdump_archive/new_taxdump_2019-09-01.zip"; fi
 if [ ! -d "new_taxdump_2019-09-01" ]; then unzip new_taxdump_2019-09-01.zip nodes.dmp names.dmp -d new_taxdump_2019-09-01; fi
 
 thapbi_pict load-tax -d "sqlite:///:memory:" -t new_taxdump_2019-09-01
 
-if [ ! -f "taxdmp_2014-08-01.zip" ]; then curl -L -O "https://ftp.ncbi.nih.gov/pub/taxonomy/taxdump_archive/taxdmp_2014-08-01.zip"; fi
-if [ ! -d "taxdmp_2014-08-01" ]; then unzip taxdmp_2014-08-01.zip nodes.dmp names.dmp -d taxdmp_2014-08-01; fi
+# Same taxonomy as database/build_ITS1_DB.sh via tests/test_build_db.sh (old style, ~50MB)
+export `grep ^TAX= database/build_ITS1_DB.sh`
+if [ ! -f "${TAX}.zip" ]; then curl -L -O "https://ftp.ncbi.nih.gov/pub/taxonomy/taxdump_archive/${TAX}.zip"; fi
+if [ ! -d "database/${TAX}" ]; then unzip ${TAX}.zip nodes.dmp names.dmp -d database/${TAX}; fi
 
 # Defaults to all Oomycetes
-thapbi_pict load-tax -d "sqlite:///:memory:" -t taxdmp_2014-08-01
+thapbi_pict load-tax -d "sqlite:///:memory:" -t "database/${TAX}"
 
 # Request Phytophthora and Peronospora
-thapbi_pict load-tax -d sqlite:///:memory: -t taxdmp_2014-08-01 -a 4783,70742
+thapbi_pict load-tax -d sqlite:///:memory: -t "database/${TAX}" -a 4783,70742
 
 # Request all Peronosporales
-thapbi_pict load-tax -d sqlite:///:memory: -t taxdmp_2014-08-01 -a 4776
+thapbi_pict load-tax -d sqlite:///:memory: -t "database/${TAX}" -a 4776
 
 # 6231 = Nematoda
-thapbi_pict load-tax -d sqlite:///:memory: -t taxdmp_2014-08-01 -a 6231
-# thapbi_pict load-tax -d sqlite:///:memory: -t new_taxdump_2019-09-01 -a 6231
+thapbi_pict load-tax -d sqlite:///:memory: -t "database/${TAX}" -a 6231
 
 # Check this error condition
 set +o pipefail
-thapbi_pict load-tax -d sqlite:///:memory: -t taxdmp_2014-08-01 -a 12908 2>&1 | grep "Could not identify any genus names"
+thapbi_pict load-tax -d sqlite:///:memory: -t "database/${TAX}" -a 12908 2>&1 | grep "Could not identify any genus names"
 set -o pipefail
 
 echo "$0 - test_load-tax.sh passed"
