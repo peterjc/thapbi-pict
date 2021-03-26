b72a9b707df2f6e91c0440cf273075fd4c9ee19e
Peter Cock <p.j.a.cock@googlemail.com>
2021-03-26 10:50:24 +0000

    Include positive and negative controls explicitly

--- examples/british_soil/run.sh
+++ examples/british_soil/run.sh
@@ -11,6 +11,6 @@ echo ===================
 # These primers are the same as our defaults, so can use our default DB.
 # Negative controls give confidence we can lower the default min abundance.
 
-thapbi_pict pipeline -i raw_data/ -s intermediate/ \
+thapbi_pict pipeline -i raw_data/ expected/ -s intermediate/ \
             -o summary/ -r british_soil_its1 -a 50 \
             -t metadata.tsv -c 1,2,3,4,5,6,7 -x 8
--- examples/british_soil/setup.sh
+++ examples/british_soil/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eup pipeline
 
-mkdir -p intermediate/ summary/
+mkdir -p intermediate/ tmp_merged/ summary/ expected/
 
 # ITS1 = PRJNA690943, COI-5P = PRJNA691575
 for ACC in `grep ITS PRJNA690943.tsv | cut -f 1`; do
@@ -9,7 +9,7 @@ for ACC in `grep ITS PRJNA690943.tsv | cut -f 1`; do
     # Column 5 should have two URLs (R1 and R2), semi-colon separated:
     for URL in `grep ^$ACC PRJNA690943.tsv | cut -f 5 | sed "s/;/ /g"`; do
         FILE=raw_data/${URL##*/}
-        # Avoiding leaving partial FASTQ if wget is interupted
+        # Avoiding leaving partial FASTQ if wget is interrupted
         rm -rf $FILE.tmp
         if [ -f $FILE ]; then
             echo "Already have $FILE"
@@ -19,4 +19,31 @@ for ACC in `grep ITS PRJNA690943.tsv | cut -f 1`; do
             mv "${FILE}.tmp" "${FILE}"
         fi
     done
+done
+
+# There are three positive controls:
+for ACC in `grep "Control Phytophthora" PRJNA690943.tsv | cut -f 1`; do
+    # echo "Setting up expected classification"
+    FILE=expected/$ACC.known.tsv
+    if [ -f $FILE ]; then
+        echo "Already have $FILE"
+    else
+        echo "Linking $FILE to mock community control"
+        cd expected/
+        ln -s ../mock_community.known.tsv $ACC.known.tsv
+        cd ..
+    fi
+done
+
+# There are two negative controls:
+for ACC in `grep "Negative" PRJNA690943.tsv | cut -f 1`; do
+    FILE=expected/$ACC.known.tsv
+    if [ -f $FILE ]; then
+        echo "Already have $FILE"
+    else
+        echo "Linking $FILE to negative control"
+        cd expected/
+        ln -s ../negative_control.known.tsv $ACC.known.tsv
+        cd ..
+    fi
 done
