2acf20dc262845fa689d6255b357456aace2a43e
Peter Cock <p.j.a.cock@googlemail.com>
2021-04-02 15:44:17 +0100

    drained lakes metadata
    This is a composite of the ENA metadata and Supplementary Table S2
    from the paper, cross-referenced on the sample_alias vs samples
    fields respectively.

--- examples/drained_ponds/run.sh
+++ examples/drained_ponds/run.sh
@@ -30,6 +30,6 @@ thapbi_pict pipeline -i raw_data/ expected/ -s intermediate/ \
             -o summary/ -r drained_ponds \
             --minlen 80 --maxlen 130 -a 50 -d NCBI_12S.sqlite --showdb \
             --left ACTGGGATTAGATACCCC --right TAGAACAGGCTCCTCTAG \
-            -t PRJNA638011.tsv -x 1 -c 4,5
+            -t metadata.tsv -x 1 -c 5,6,7,8,9,10,4,3
 
 echo "Done"
