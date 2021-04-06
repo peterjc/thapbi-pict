8c80a50ae93b74b707c656cd12966badc58fed9a
Peter Cock <p.j.a.cock@googlemail.com>
2021-04-06 10:37:53 +0100

    Update assess test

--- tests/test_assess.sh
+++ tests/test_assess.sh
@@ -19,21 +19,25 @@ set -x
 thapbi_pict assess 2>&1 | grep "the following arguments are required"
 set -o pipefail
 
+export DB=$TMP/seven.sqlite
+rm -rf $DB
+thapbi_pict curated-import -d $DB -i tests/assess/seven.fasta -x
+
 # Simple examples with expected output to compare against
 for SAMPLE in ex1 ex2 ex3 ex4 unclassified fp; do
-    thapbi_pict assess -m identity -i tests/assess/$SAMPLE.known.tsv tests/assess/$SAMPLE.identity.tsv -o $TMP/$SAMPLE.assess.tsv
+    thapbi_pict assess -m identity -i tests/assess/$SAMPLE.known.tsv tests/assess/$SAMPLE.identity.tsv -o $TMP/$SAMPLE.assess.tsv -d $DB
     diff $TMP/$SAMPLE.assess.tsv tests/assess/$SAMPLE.assess.tsv
 done
 
 echo "Checking all classifier assessment outputs"
-thapbi_pict assess -i tests/assess/ -o $TMP/assess.tsv -t $TMP/tally.tsv -c $TMP/confusion.tsv -m identity
+thapbi_pict assess -i tests/assess/ -o $TMP/assess.tsv -t $TMP/tally.tsv -c $TMP/confusion.tsv -m identity -d $DB
 diff $TMP/tally.tsv tests/assess/samples.tally.tsv
 diff $TMP/assess.tsv tests/assess/samples.assess.tsv
 diff $TMP/confusion.tsv tests/assess/samples.confusion.tsv
 
 echo "Checking warning for unexpected species"
 set +o pipefail
-thapbi_pict assess -m identity --input tests/assess/*.identity.tsv tests/assess/*.known.tsv 2>&1 | grep "WARNING: 1 expected species were not a possible prediction: Phytophthora fallax"
+thapbi_pict assess -d $DB -m identity --input tests/assess/*.identity.tsv tests/assess/*.known.tsv 2>&1 | grep "WARNING: 1 expected species were not a possible prediction: Phytophthora fallax"
 set -o pipefail
 
 if [ ! -f $TMP/thapbi_swarm/DNAMIX_S95_L001.swarm.tsv ]; then echo "Run test_classify.sh to setup test input"; false; fi
@@ -42,6 +46,8 @@ if [ ! -f $TMP/DNAMIX_S95_L001.identity.tsv ]; then echo "Run test_classify.sh t
 rm -rf $TMP/assess_swarm_vs_identity.tsv
 rm -rf $TMP/confusion_swarm_vs_identity.tsv
 
+echo "Testing swarm vs identity"
+# Note using the default DB here, as would be used in test_classify.sh to generate inputs
 # Don't have a gold standard known truth to test this against, so test swarm vs identity
 thapbi_pict assess -i $TMP/thapbi_swarm/DNAMIX_S95_L001.swarm.tsv $TMP/DNAMIX_S95_L001.identity.tsv -m swarm -k identity -o $TMP/assess_swarm_vs_identity.tsv -c $TMP/confusion_swarm_vs_identity.tsv
 
