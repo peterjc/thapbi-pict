ecbc40d58a35a7abd3c11ec7db3a640b245efaaa
Peter Cock <p.j.a.cock@googlemail.com>
2021-04-08 10:42:56 +0100

    Revise fungal_mock example folder names
    Trying to be more standardised here

--- examples/fungal_mock/run.sh
+++ examples/fungal_mock/run.sh
@@ -16,21 +16,22 @@ function analyse {
     # Using minimum of 2 gives 75k unique, 5 gives 22k, and 10 gives 8.8k unique.
     # Using minimum of 100 (default) gives under 800 unique over samples.
     # [Counts were over both amplicons using the actual primer pairs, 3 runs]
-    mkdir -p $LIBRARY/intermediate/$NAME
+    mkdir -p intermediate/${LIBRARY}_${NAME}/
     for METHOD in identity onebp blast; do
         thapbi_pict pipeline -d ${NAME}.sqlite --left $LEFT --right $RIGHT \
-                    -i $LIBRARY/raw_data/ $LIBRARY/expected/ -m $METHOD \
-                    -s $LIBRARY/intermediate/$NAME/ -o $LIBRARY/summary/ \
-                    -r $LIBRARY.$NAME -a 10 \
-                    --showdb -t $LIBRARY/metadata.tsv -c 5,6,7,3,4,2 -x 1 -g 6
+                    -i raw_data/$LIBRARY/ expected/$LIBRARY/ -m $METHOD \
+                    -s intermediate/${LIBRARY}_${NAME}/ -o summary/ \
+                    -r ${LIBRARY}_${NAME} -a 10 \
+                    --showdb -t metadata_$LIBRARY.tsv -c 5,6,7,3,4,2 -x 1 -g 6
     done
     # Now run an edit-graph at a higher abundance threshold
     # (works as long as pipeline or prepare-reads was run with
     # the same or lower threshold).
     # Including all DB entries with -s / --showdb argument
     # Do not show the classifier output using -m with "-"
-    thapbi_pict edit-graph -d ${NAME}.sqlite -i $LIBRARY/intermediate/$NAME/ --showdb \
-                -o $LIBRARY/summary/$LIBRARY.$NAME.edit-graph.a75.xgmml -a 75 -m -
+    thapbi_pict edit-graph -d ${NAME}.sqlite --showdb \
+                -i intermediate/${LIBRARY}_${NAME}/ -a 75 -m - \
+                -o summary/${LIBRARY}_${NAME}.edit-graph.a75.xgmml
     echo "$NAME done"
 }
 
@@ -39,7 +40,7 @@ echo Amplicon library one - ITS1 - BITS/B58S3 primers
 echo ================================================
 
 MARKER=ITS1
-LIBRARY=amp_lib_one
+LIBRARY=AL1
 NAME=BITS_B58S3
 LEFT=ACCTGCGGARGGATC
 RIGHT=GAGATCCRTTGYTRAAAGTT
@@ -54,7 +55,7 @@ echo really used ITS1f/ITS2 primers which amplify a
 echo a larger fragment, see below.
 
 MARKER=ITS1
-LIBRARY=amp_lib_two
+LIBRARY=AL2
 NAME=BITS_B58S3
 LEFT=ACCTGCGGARGGATC
 RIGHT=GAGATCCRTTGYTRAAAGTT
@@ -66,7 +67,7 @@ echo Amplicon library two - ITS1 - ITS1f/ITS2 primers
 echo ================================================
 
 MARKER=ITS1
-LIBRARY=amp_lib_two
+LIBRARY=AL2
 NAME=ITS1f_ITS2
 LEFT=CTTGGTCATTTAGAGGAAGTAA
 RIGHT=GCTGCGTTCTTCATCGATGC
@@ -78,7 +79,7 @@ echo Amplicon library two - ITS2 - ITS3-KYO2/ITS4-KYO3 primers
 echo =========================================================
 
 MARKER=ITS2
-LIBRARY=amp_lib_two
+LIBRARY=AL2
 NAME=ITS3-KYO2_ITS4-KYO3
 LEFT=GATGAAGAACGYAGYRAA
 RIGHT=CTBTTVCCKCTTCACTCG
