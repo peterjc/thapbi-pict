8f8a982766931fcde48552bbd09563959fb713c1
Peter Cock <p.j.a.cock@googlemail.com>
2021-05-20 14:14:44 +0100

    Should remove XGMML files to regenerate them

--- examples/clean_all.sh
+++ examples/clean_all.sh
@@ -7,8 +7,13 @@ for example in woody_hosts recycled_water fungal_mock microalgal_mock great_lake
     echo "========================="
     echo "Cleaning $example"
     echo "========================="
+    rm -rf $example/*.sqlite $example/*/*.sqlite
+
     # Do not touch woody_hosts/intermediate.tar.bz2
-    rm -rf $example/*.sqlite $example/*/*.sqlite $example/intermediate $example/intermediate_* $example/tmp_merged
+    rm -rf $example/intermediate $example/intermediate_* $example/tmp_merged
+
+    # The XGMML files are not overwritten by default
+    rm -rf $example/summary/*.xgmml
     echo "Done"
 done
 
