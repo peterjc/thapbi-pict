9a1c0f30e94de66cf931967eafa9c4cb9e0eb41a
Peter Cock <p.j.a.cock@googlemail.com>
2021-06-09 14:40:36 +0100

    Scripts for running this soil nematodes example

---
+++ examples/soil_nematodes/run.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+set -eup pipeline
+
+echo NOTE: Expected first time run time is about XX minutes,
+echo repeat runs about 1 minute just to regenerate reports.
+echo
+
+mkdir -p intermediate/ summary/
+
+ILL_LEFT=TCGTCGGCAGCGTCAGATGTGTATAAGAGACAG
+ILL_RIGHT=GTCTCGTGGGCTCGGAGATGTGTATAAGAGACAG
+
+# Quoting table 2:
+#
+# Primer Sequence (from 5ʹ end) Source
+# NF1  GGTGGTGCATGGCCGTTCTTAGTT Porazinska et al. 2009
+# 18Sr2b TACAAAGGGCAGGGACGTAAT
+# SSUF04  GCTTGTCTCAAAGATTAAGCC Blaxter et al. 1998
+# SSUR22 GCCTGCTGCCTTCCTTGGA
+# D3FA  GACCCGTCTTGAAACACGGA Nunn 1992
+# D3BR CGGAAGGAACCAGCTACTA
+# JB3 TTTTTTGGGCATCCTGAGGTTTAT Bowles et al. 1992
+# JB5GED  AGCACCTAAACTTAAAACATARTGRAARTG Derycke et al. 2010
+
+
+# Takes arguments via variable names
+function analyse {
+    if [ ! -f references/$NAME.sqlite ]; then
+        echo "Making DB $NAME.sqlite"
+        thapbi_pict import -d references/$NAME.sqlite \
+                    -i references/$NAME.fasta -s ";" -x
+    fi
+
+    echo "Running analysis"
+    mkdir -p intermediate/$NAME/
+    thapbi_pict pipeline -d references/$NAME.sqlite --merged-cache tmp_merged/ \
+                 --left $ILL_LEFT$LEFT --right $ILL_RIGHT$RIGHT \
+                -i raw_data/ expected/$NAME/ -s intermediate/$NAME/ \
+                -o summary -r $NAME -t metadata.tsv -x 1 -c 4,3
+
+    echo "$NAME done"
+}
+
+echo ========================
+echo NF1/18Sr2b
+echo ========================
+
+NAME=NF1-18Sr2b
+LEFT=GGTGGTGCATGGCCGTTCTTAGTT
+RIGHT=TACAAAGGGCAGGGACGTAAT
+
+analyse  # call function above
+
+echo ========================
+echo SSUF04/SSUR22
+echo ========================
+
+NAME=SSUF04-SSUR22
+LEFT=GCTTGTCTCAAAGATTAAGCC
+RIGHT=GCCTGCTGCCTTCCTTGGA
+
+analyse  # call function above
+
+echo ========================
+echo D3Af/D3Br
+echo ========================
+
+NAME=D3Af-D3Br
+LEFT=GACCCGTCTTGAAACACGGA
+RIGHT=CGGAAGGAACCAGCTACTA
+
+analyse  # call function above
+
+echo ========================
+echo JB3/JB5GED
+echo ========================
+
+NAME=JB3-JB5GED
+LEFT=TTTTTTGGGCATCCTGAGGTTTAT
+RIGHT=AGCACCTAAACTTAAAACATARTGRAARTG
+
+analyse  # call function above
+
+echo ====
+echo Done
+echo ====
---
+++ examples/soil_nematodes/setup.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+set -eup pipeline
+
+mkdir -p raw_data/ expected/ tmp_merged/ intermediate/ intermediate_pool/ summary/
+if [ ! -f raw_data/MD5SUM.txt ]; then
+    echo "ERROR: Missing raw_data/MD5SUM.txt"
+    #false
+fi
+
+echo "=========================="
+echo "Downloading reads from ENA"
+echo "=========================="
+for ACC in `grep ^ERR PRJEB27581.tsv | cut -f 1`; do
+    # echo "Downloading $ACC"
+    # Column 4 should have two URLs (R1 and R2), semi-colon separated:
+    for URL in `grep ^$ACC PRJEB27581.tsv | cut -f 4 | sed "s/;/ /g"` ; do
+        NAME=${URL##*/}
+        FILE=raw_data/$NAME
+        # Avoiding leaving partial FASTQ if wget is interupted
+        rm -rf $FILE.tmp
+        if [ -f $FILE ]; then
+            echo "Already have $FILE"
+        else
+            echo "Downloading $FILE"
+            wget -O "${FILE}.tmp" "ftp://$URL"
+            mv "${FILE}.tmp" "${FILE}"
+        fi
+    done
+    # Beware 1 vs I typo in ENA metadata for NF1-18Sr2b in PRJEB27581.tsv
+    # The paper and our metadata.tsv uses the digit 1.
+    for MARKER in NF1-18Sr2b SSUF04-SSUR22 D3Af-D3Br JB3-JB5GED; do
+        mkdir -p expected/$MARKER/
+        if [ ! -f expected/$MARKER/$ACC.known.tsv ]; then
+            echo "Creating link for expected/$MARKER/$ACC.known.tsv"
+            cd expected/$MARKER/
+            if [[ `grep $ACC ../../metadata.tsv | cut -f 3` == "Blank" ]]; then
+                ln -s ../../negative_control.known.tsv $ACC.known.tsv
+            elif [[ `grep $ACC ../../metadata.tsv | cut -f 4` == "$MARKER" ]]; then
+                ln -s ../../mock_community.known.tsv $ACC.known.tsv
+            else
+                # Should not contain this marker
+                ln -s ../../negative_control.known.tsv $ACC.known.tsv
+            fi
+            cd ../..
+        fi
+    done
+done
+
+echo "=========="
+echo "Setup done"
+echo "=========="
