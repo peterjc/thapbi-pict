4967989f6fc0d64af0633899671c1f310f3f1758
Peter Cock <p.j.a.cock@googlemail.com>
2021-06-21 10:02:41 +0100

    Test pooling.sh help

--- tests/test_pooling.sh
+++ tests/test_pooling.sh
@@ -7,7 +7,8 @@
 # file that should have been included as part of this package.
 
 IFS=$'\n\t'
-set -euo pipefail
+set -eu
+# Note not using "set -o pipefail" until after check error message with grep
 
 export TMP=${TMP:-/tmp/thapbi_pict}/classify
 mkdir -p $TMP
@@ -15,6 +16,9 @@ mkdir -p $TMP
 echo "==================="
 echo "Checking pooling.py"
 echo "==================="
+set -x
+scripts/pooling.py -h 2>&1 | grep "Replace read counts with boolean"
+set -o pipefail
 
 scripts/pooling.py -i tests/pooling/example.samples.onebp.tsv -c 2,3,4,5 --pcr -z -p 6 -o $TMP/example
 diff $TMP/example.tsv tests/pooling/example.pooled.tsv
