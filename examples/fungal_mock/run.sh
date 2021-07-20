3196dc4a5b14f5904229105d081cec94b7c71d86
Peter Cock <p.j.a.cock@googlemail.com>
2021-07-20 14:25:05 +0100

    Refactor fungal_mock to use multi-marker work

--- examples/fungal_mock/run.sh
+++ examples/fungal_mock/run.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 set -euo pipefail
 
-echo "NOTE: Expected first time run time is about 5 minutes,"
-echo "repeat runs under a minute just to regenerate reports."
+echo "NOTE: Expected first time run time is under 5 minutes,"
+echo "repeat runs under a minute to just to regenerate reports."
 echo
 
 mkdir -p intermediate/ summary/
 
 # Takes arguments via variable names
-function analyse {
-    if [ ! -f ${NAME}.sqlite ]; then
-        echo "Trimming $MARKER sequences for $NAME"
-        export RIGHT_RC=`python -c "from Bio.Seq import reverse_complement as rc; print(rc('$RIGHT'))"`
-        # Doing the left and right primer trimming separately:
-        cutadapt --quiet -g $LEFT $MARKER.fasta \
-          | cutadapt --quiet -a $RIGHT_RC -o $NAME.fasta /dev/stdin
-        echo "Building $MARKER database for $NAME"
-        thapbi_pict import -d $NAME.sqlite -i $NAME.fasta  -x \
-                    -k $NAME --left $LEFT --right $RIGHT
-    fi
+function import_marker {
+    echo "Trimming $GENE sequences for $MARKER"
+    export RIGHT_RC=`python -c "from Bio.Seq import reverse_complement as rc; print(rc('$RIGHT'))"`
+    # Doing the left and right primer trimming separately:
+    cutadapt --quiet -g $LEFT $GENE.fasta \
+      | cutadapt --quiet -a $RIGHT_RC -o $MARKER.fasta /dev/stdin
+    echo "Adding $GENE $MARKER to database"
+    thapbi_pict import -d $DB -i $MARKER.fasta  -x \
+                -k $MARKER --left $LEFT --right $RIGHT
+}
 
+# Takes arguments via variable names
+function analyse {
     echo "Running analysis with minimum abundance threshold ten"
     # No threshold (-a 0 or -a 1) gives 450k total unique entries over samples
     # Using minimum of 2 gives 75k unique, 5 gives 22k, and 10 gives 8.8k unique.
@@ -27,34 +28,50 @@ function analyse {
     # [Counts were over both amplicons using the actual primer pairs, 3 runs]
     mkdir -p intermediate/${LIBRARY}/
     for METHOD in identity onebp blast; do
-        thapbi_pict pipeline -d ${NAME}.sqlite \
-                    -i raw_data/$LIBRARY/ expected/$LIBRARY/ -m $METHOD \
+        thapbi_pict pipeline -d $DB -m $METHOD \
+                    -i raw_data/$LIBRARY/ expected/$LIBRARY/ \
                     -s intermediate/${LIBRARY}/ \
                     -o summary/${LIBRARY} -a 10 \
                     -t metadata_$LIBRARY.tsv -c 5,6,7,3,4,2 -x 1 -g 6
     done
+    echo "$LIBRARY done"
+}
+
+# Takes arguments via variable names
+function edit_graph {
     # Now run an edit-graph at a higher abundance threshold
     # (works as long as pipeline or prepare-reads was run with
     # the same or lower threshold).
     # Including all DB entries with -s / --showdb argument
     # Do not show the classifier output using -m with "-"
-    thapbi_pict edit-graph -d ${NAME}.sqlite --showdb \
-                -i intermediate/${LIBRARY}/${NAME}/ -a 75 -m - \
-                -o summary/${LIBRARY}_${NAME}.edit-graph.a75.xgmml
-    echo "$NAME done"
+    thapbi_pict edit-graph -d $DB --showdb \
+                -i intermediate/${LIBRARY}/${MARKER}/ -a 75 -m - \
+                -o summary/${LIBRARY}.${MARKER}.edit-graph.a75.xgmml
 }
 
-echo ================================================
-echo Amplicon library one - ITS1 - BITS/B58S3 primers
-echo ================================================
 
-MARKER=ITS1
+DB=fungi_solo.sqlite
+if [ ! -f $DB ]; then
+    echo ==================================================
+    echo Creating database - ITS1 - BITS/B58S3 primers only
+    echo ==================================================
+
+    GENE=ITS1
+    MARKER=BITS-B58S3
+    LEFT=ACCTGCGGARGGATC
+    RIGHT=GAGATCCRTTGYTRAAAGTT
+    import_marker  # call function above
+fi
+
+echo =====================================================
+echo Amplicon library one - ITS1 - BITS/B58S3 primers only
+echo =====================================================
+
 LIBRARY=AL1
-NAME=BITS-B58S3
-LEFT=ACCTGCGGARGGATC
-RIGHT=GAGATCCRTTGYTRAAAGTT
+analyse  # call function above
 
-analyse # call function above
+MARKER=BITS-B58S3
+edit_graph  # call function abover
 
 echo ================================================
 echo Amplicon library two - ITS1 - BITS/B58S3 primers
@@ -63,37 +80,44 @@ echo Note: This is a blinkered view of this dataset,
 echo really used ITS1f/ITS2 primers which amplify a
 echo a larger fragment, see below.
 
-MARKER=ITS1
 LIBRARY=AL2
-NAME=BITS-B58S3
-LEFT=ACCTGCGGARGGATC
-RIGHT=GAGATCCRTTGYTRAAAGTT
+analyse  # call function above
 
-analyse # call function above
+MARKER=BITS-B58S3
+edit_graph  # call function abover
+
+DB=fungi_duo.sqlite
+if [ ! -f $DB ]; then
+    echo =================================
+    echo Creating database - ITS1 and ITS2
+    echo =================================
+
+    GENE=ITS1
+    MARKER=ITS1f-ITS2
+    LEFT=CTTGGTCATTTAGAGGAAGTAA
+    RIGHT=GCTGCGTTCTTCATCGATGC
+    import_marker  # call function above
+
+    GENE=ITS2
+    LIBRARY=AL2
+    MARKER=ITS3-KYO2-ITS4-KYO3
+    LEFT=GATGAAGAACGYAGYRAA
+    RIGHT=CTBTTVCCKCTTCACTCG
+    import_marker  # call function above
+fi
 
 echo ================================================
-echo Amplicon library two - ITS1 - ITS1f/ITS2 primers
+echo Amplicon library two, with two primers products:
+echo ITS1 - ITS1f/ITS2 primers
+echo ITS2 - ITS3-KYO2/ITS4-KYO3 primers
 echo ================================================
 
-MARKER=ITS1
 LIBRARY=AL2
-NAME=ITS1f-ITS2
-LEFT=CTTGGTCATTTAGAGGAAGTAA
-RIGHT=GCTGCGTTCTTCATCGATGC
+analyse  # call function above
 
-analyse # call function above
-
-echo =========================================================
-echo Amplicon library two - ITS2 - ITS3-KYO2/ITS4-KYO3 primers
-echo =========================================================
-
-MARKER=ITS2
-LIBRARY=AL2
-NAME=ITS3-KYO2-ITS4-KYO3
-LEFT=GATGAAGAACGYAGYRAA
-RIGHT=CTBTTVCCKCTTCACTCG
-
-analyse # call function above
+for MARKER in ITS1f-ITS2 ITS3-KYO2-ITS4-KYO3; do
+    edit_graph  # call function above
+done
 
 echo ====
 echo Done
