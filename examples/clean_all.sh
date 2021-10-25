ea9182bd7e6ac59058167794d96c770799f6eafb
Peter Cock <p.j.a.cock@googlemail.com>
2021-10-25 14:26:42 +0100

    Remove 'intermediate' all_reads classifier TSV
    This used to be under intermediate/ up until v0.9.9

--- examples/clean_all.sh
+++ examples/clean_all.sh
@@ -15,6 +15,9 @@ for example in `grep "/index" ../docs/examples/index.rst | cut -f 1 -d "/" | cut
     # Do not touch woody_hosts/intermediate.tar.bz2
     rm -rf $example/intermediate $example/intermediate_*
 
+    # The classifier TSV is not (currently) overwritten by default:
+    rm -rf $example/summary/*.all_reads.*.tsv
+
     # The XGMML files are not overwritten by default
     rm -rf $example/summary/*.xgmml
     echo "Done"
