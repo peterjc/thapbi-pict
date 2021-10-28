93d25868bdbccb8ede42aba2303a38d6dd8fc912
Peter Cock <p.j.a.cock@googlemail.com>
2021-10-28 18:12:55 +0100

    Check species entry for Hyaloperonospora parasitica species group

--- tests/test_load-tax.sh
+++ tests/test_load-tax.sh
@@ -43,7 +43,10 @@ if [ `sqlite3 $DB "SELECT COUNT(*) FROM synonym;"` -ne 4 ]; then echo "Wrong syn
 if [ `sqlite3 $DB "SELECT COUNT(synonym.id) FROM synonym JOIN taxonomy ON synonym.taxonomy_id==taxonomy.id WHERE taxonomy.genus='Nothophytophthora' AND taxonomy.species='';"` -ne 4 ]; then echo "Wrong synonym target"; false; fi
 
 # Defaults to all Oomycetes
-thapbi_pict load-tax -d "sqlite:///:memory:" -t "database/${TAX}"
+export DB=$TMP/Oomycetes.sqlite
+thapbi_pict load-tax -d $DB -t "database/${TAX}"
+if [ `sqlite3 $DB "SELECT COUNT(id) FROM taxonomy WHERE genus='Hyaloperonospora' AND species='parasitica species group'"` -ne 1 ]; then echo "Missing species entry for Hyaloperonospora parasitica species group"; false; fi
+if [ `sqlite3 $DB "SELECT COUNT(synonym.name) FROM synonym JOIN taxonomy WHERE synonym.taxonomy_id==taxonomy.id AND taxonomy.genus='Hyaloperonospora' AND taxonomy.species='parasitica species group'"` -ne 16 ]; then echo "Wrong synonym count for Hyaloperonospora parasitica species group";false; fi
 
 # Request Phytophthora and Peronospora
 thapbi_pict load-tax -d sqlite:///:memory: -t "database/${TAX}" -a 4783,70742
