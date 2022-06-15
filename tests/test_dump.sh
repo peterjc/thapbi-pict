dc0a0d8334718c7a17b056bcd07a272a3c7562a0
Peter Cock <p.j.a.cock@googlemail.com>
2022-06-15 13:16:27 +0100

    Update tests for 3 more seqs in DB

--- tests/test_dump.sh
+++ tests/test_dump.sh
@@ -19,15 +19,15 @@ set -x
 thapbi_pict dump -s fallax 2>&1 | grep "species requires a single genus"
 set -o pipefail
 
-if [ `thapbi_pict dump | grep -c -v "^#"` -ne 3599 ]; then echo "Wrong source count for table dump"; false; fi
-if [ `thapbi_pict dump -f fasta | grep -c "^>"` -ne 1263 ]; then echo "Wrong sequence count for fasta dump"; false; fi
+if [ `thapbi_pict dump | grep -c -v "^#"` -ne 3602 ]; then echo "Wrong source count for table dump"; false; fi
+if [ `thapbi_pict dump -f fasta | grep -c "^>"` -ne 1266 ]; then echo "Wrong sequence count for fasta dump"; false; fi
 
-if [ `thapbi_pict dump --minimal | grep -c -v "^#"` -ne 1263 ]; then echo "Wrong sequence count for minimal table dump"; false; fi
-if [ `thapbi_pict dump -m -f fasta | grep -c "^>"` -ne 1263 ]; then echo "Wrong sequence count for minimal fasta dump"; false; fi
+if [ `thapbi_pict dump --minimal | grep -c -v "^#"` -ne 1266 ]; then echo "Wrong sequence count for minimal table dump"; false; fi
+if [ `thapbi_pict dump -m -f fasta | grep -c "^>"` -ne 1266 ]; then echo "Wrong sequence count for minimal fasta dump"; false; fi
 
 # With genus filter,
-if [ `thapbi_pict dump -f txt -g Phytophthora | grep -v -c "^#"` -ne 2582 ]; then echo "Wrong source for Phytophthora genus"; false; fi
-if [ `thapbi_pict dump -f fasta -g Phytophthora -m | grep -c "^>"` -ne 793 ]; then echo "Wrong sequence for Phytophthora genus"; false; fi
+if [ `thapbi_pict dump -f txt -g Phytophthora | grep -v -c "^#"` -ne 2585 ]; then echo "Wrong source for Phytophthora genus"; false; fi
+if [ `thapbi_pict dump -f fasta -g Phytophthora -m | grep -c "^>"` -ne 796 ]; then echo "Wrong sequence for Phytophthora genus"; false; fi
 
 # With genus and species filter,
 if [ `thapbi_pict dump -f txt -g Phytophthora -s "fallax, andina" | grep -v -c "^#"` -ne 7 ]; then echo "Wrong source for two species"; false; fi
