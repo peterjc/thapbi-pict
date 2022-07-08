6ce443c85df22d3d8e198acbc55521914445ec05
Peter Cock <p.j.a.cock@googlemail.com>
2022-07-08 12:48:24 +0100

    Switch to ObiTools style FASTA to use taxid and species

--- examples/oomycete_rps10/rename_fasta.py
+++ examples/oomycete_rps10/rename_fasta.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 """Rename entries in OomyceteDB style FASTA file (via stdin/stdout).
 
+Outputs a FASTA file in the ObiTools style to capture both the
+species name and the NCBI taxid.
+
 NOTE: Does not preserve sequence case or any line breaks, gets converted to
 upper case as a single line.
 """
@@ -10,5 +13,6 @@ from Bio.SeqIO.FastaIO import SimpleFastaParser
 
 for title, seq in SimpleFastaParser(sys.stdin):
     species = title.rstrip().rsplit(";")[-1].replace("_", " ")
-    idn = title[title.find("|oodb_id=") :].split("|", 2)[1]
-    sys.stdout.write(f">{idn} {species}\n{seq}\n")
+    idn = title[title.index("|oodb_id=") :].split("|", 2)[1]
+    taxid = title[title.index("|ncbi_taxid=") + 12 :].split("|", 1)[0]
+    sys.stdout.write(f">{idn} species_name={species}; taxid={taxid};\n{seq}\n")
