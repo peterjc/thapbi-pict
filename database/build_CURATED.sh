f2ade394253241457860d891b2dd349a08690969
Peter Cock <p.j.a.cock@googlemail.com>
2022-07-27 13:29:00 +0100

    Don't need this hand-curated list of Pythium to ignore

--- database/build_CURATED.sh
+++ database/build_CURATED.sh
@@ -64,9 +64,6 @@ thapbi_pict import -d "$DB.sqlite" -i controls.fasta \
 # sqlite3 "$DB.sqlite" "DELETE FROM sequence_source WHERE source_accession IN ('GQ149496.1', 'JF916542.1');"
 # KY986679 probably not Peronospora erucastri, but Hyaloperonospora
 # sqlite3 "$DB.sqlite" "DELETE FROM sequence_source WHERE source_accession IN ('KY986679.X');"
-# Assorted entries, mostly Pythium which probably should be Globisporangium or Phytopythium etc
-# BAD="('MK794853.1', 'MK794848.1', 'MK795051.1', 'KY785380.1', 'KY785381.1', 'KU715054.1', 'MK794725.1', 'MK794726.1', 'ON394668.1', 'ON394667.1', 'ON394669.1', 'KU210557.1', 'ON075205.1', 'MZ799354.1', 'MZ799355.1', 'LC684551.1', 'ON394664.1', 'ON394670.1', 'KY785379.1', 'KU715059.1', 'MK794740.1', 'MK794746.1', 'ON394673.1')"
-# sqlite3 "$DB.sqlite" "DELETE FROM sequence_source WHERE source_accession IN $BAD;"
 # Many of the Pythium entries appear to now be Globisporangium or Phytopythium,
 # simplest to exclude them all for now...
 # sqlite3 ITS1_DB.sqlite "DELETE FROM sequence_source WHERE id in (SELECT sequence_source.id FROM sequence_source JOIN taxonomy WHERE sequence_source.taxonomy_id = taxonomy.id AND taxonomy.genus='Pythium' AND taxonomy.species='');"
