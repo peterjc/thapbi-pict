fc85be518f9321d2a3747a6730d113fb0c46b5f0
Peter Cock <p.j.a.cock@googlemail.com>
2023-11-16 15:06:45 +0000

    Drop variants of DB build script
    This would better done with if-statements in a single core script

--- database/build_all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-VERSION=`thapbi_pict -v | sed "s/THAPBI PICT //g"`
-set -euo pipefail
-
-./build_CURATED.sh && ./build_CURATED+NCBI.sh && ./build_ITS1_DB.sh
-
-echo "All done, used THAPBI PICT $VERSION"
