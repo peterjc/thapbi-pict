0b798e65b1bfdd2e2fc946963e5f0f59a70c8f88
Peter Cock <p.j.a.cock@googlemail.com>
2024-03-05 13:25:13 +0000

    Validate our BIOM output including that ASV names match TSV output

--- tests/test_sample-tally.sh
+++ tests/test_sample-tally.sh
@@ -21,23 +21,34 @@ set -x
 thapbi_pict sample-tally 2>&1 | grep "the following arguments are required"
 set -o pipefail
 
-rm -rf $TMP/x.tsv
-thapbi_pict sample-tally -i tests/prepare-reads/DNAMIX_S95_L001.fasta -o $TMP/x.tsv
+rm -rf $TMP/x.*
+thapbi_pict sample-tally -i tests/prepare-reads/DNAMIX_S95_L001.fasta \
+    -o $TMP/x.tsv --fasta $TMP/x.fasta
 diff $TMP/x.tsv tests/sample-tally/DNAMIX_S95_L001.tally.tsv
+# Confirm the TSV and FASTA are using the same ASV names:
+diff <(grep ^ITS1 /tmp/thapbi_pict/sample-tally/x.tsv | cut -f 1) \
+     <(grep "^>" /tmp/thapbi_pict/sample-tally/x.fasta | cut -c 2-)
 
 if ! [ -x "$(command -v biom)" ]; then
     echo 'WARNING: biom-format not installed, skipping some tests'
 else
-    for X in $TMP/x.tsv; do
-        echo "Checking biom convert works with $X"
-        biom convert -i $X -o $TMP/biom.tsv --table-type="OTU table" --to-tsv \
-             --header-key Sequence --tsv-metadata-formatter naive
-        biom convert -i $X -o $TMP/biom.hdf5 --table-type="OTU table" --to-hdf5
-        biom convert -i $X -o $TMP/biom.json --table-type="OTU table" --to-json
-        # Don't know how stable the output is, so don't use diff
-        grep -c "Biological Observation Matrix 1.0.0" $TMP/biom.json
+    thapbi_pict sample-tally -i tests/prepare-reads/DNAMIX_S95_L001.fasta -b $TMP/x.biom
 
-    done
+    echo "Checking our TSV output works with biom convert"
+    biom convert -i $TMP/x.tsv -o $TMP/biom.tsv --table-type="OTU table" --to-tsv \
+         --header-key Sequence --tsv-metadata-formatter naive
+    biom convert -i $TMP/x.tsv -o $TMP/biom.hdf5 --table-type="OTU table" --to-hdf5
+    biom convert -i $TMP/x.tsv -o $TMP/biom.json --table-type="OTU table" --to-json
+    # Don't know how stable the output is, so don't use diff
+    grep -c "Biological Observation Matrix 1.0.0" $TMP/biom.json
+
+    echo "Checking our BIOM output validates"
+    biom validate-table -i $TMP/x.biom
+    # Export the sample names
+    diff <(biom table-ids -i $TMP/x.biom) <(echo DNAMIX_S95_L001)
+    # Export the ASV names and their sequences, confirm matches our TSV
+    diff <(biom export-metadata -i $TMP/x.biom --observation-metadata-fp /dev/stdout | grep ^ITS1) \
+         <(cut -f 1,3 $TMP/x.tsv | grep ^ITS1)
 fi
 
 echo "----------------"
